dmytrokurochkin/MZGram-Desktop

commit 32c6b545d6d10a37ed6fd44f9fecf85fd83dd86d

Fix data export window theme changing.
Fixes #4902.

Telegram/SourceFiles/platform/win/main_window_win.cpp
@@ -646,10 +646,7 @@ int32 MainWindow::screenNameChecksum(const QString &name) const {
 }
 
 void MainWindow::psRefreshTaskbarIcon() {
-	auto refresher = object_ptr<QWidget>(this);
-	auto guard = gsl::finally([&refresher] {
-		refresher.destroy();
-	});
+	auto refresher = std::unique_ptr<QWidget>(this);
 	refresher->setWindowFlags(static_cast<Qt::WindowFlags>(Qt::Tool) | Qt::FramelessWindowHint);
 	refresher->setGeometry(x() + 1, y() + 1, 1, 1);
 	auto palette = refresher->palette();

Telegram/SourceFiles/ui/twidget.cpp
@@ -215,6 +215,12 @@ QImage GrabWidgetToImage(not_null<QWidget*> target, QRect rect, QColor bg) {
 	return result;
 }
 
+void ForceFullRepaint(not_null<QWidget*> widget) {
+	const auto refresher = std::make_unique<QWidget>(widget);
+	refresher->setGeometry(widget->rect());
+	refresher->show();
+}
+
 } // namespace Ui
 
 void sendSynteticMouseEvent(QWidget *widget, QEvent::Type type, Qt::MouseButton button, const QPoint &globalPoint) {

Telegram/SourceFiles/ui/twidget.h
@@ -57,6 +57,8 @@ QImage GrabWidgetToImage(
 	QRect rect = QRect(),
 	QColor bg = QColor(255, 255, 255, 0));
 
+void ForceFullRepaint(not_null<QWidget*> widget);
+
 } // namespace Ui
 
 enum class RectPart {

Telegram/SourceFiles/ui/widgets/separate_panel.cpp
@@ -17,6 +17,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/toast/toast.h"
 #include "ui/widgets/tooltip.h"
 #include "window/layer_widget.h"
+#include "window/themes/window_theme.h"
 #include "messenger.h"
 #include "styles/style_widgets.h"
 #include "styles/style_info.h"
@@ -150,6 +151,13 @@ void SeparatePanel::initLayout() {
 	setAttribute(Qt::WA_TranslucentBackground, true);
 
 	createBorderImage();
+	subscribe(Window::Theme::Background(), [=](
+			const Window::Theme::BackgroundUpdate &update) {
+		if (update.paletteChanged()) {
+			createBorderImage();
+			Ui::ForceFullRepaint(this);
+		}
+	});
 
 	Platform::InitOnTopPanel(this);
 }

Telegram/SourceFiles/ui/widgets/separate_panel.h
@@ -23,7 +23,7 @@ class LayerStackWidget;
 
 namespace Ui {
 
-class SeparatePanel : public Ui::RpWidget {
+class SeparatePanel : public Ui::RpWidget, private base::Subscriber {
 public:
 	SeparatePanel();
 

Telegram/SourceFiles/window/main_window.cpp
@@ -60,9 +60,6 @@ MainWindow::MainWindow()
 , _titleText(qsl("Telegram")) {
 	subscribe(Theme::Background(), [this](const Theme::BackgroundUpdate &data) {
 		if (data.paletteChanged()) {
-			if (_title) {
-				_title->update();
-			}
 			updatePalette();
 		}
 	});
@@ -242,6 +239,8 @@ void MainWindow::handleActiveChanged() {
 }
 
 void MainWindow::updatePalette() {
+	Ui::ForceFullRepaint(this);
+
 	auto p = palette();
 	p.setColor(QPalette::Window, st::windowBg->c);
 	setPalette(p);

commit ed76fda6d8fd60f1c1bad97469451fa08d64ee9c

Fix crash in taskbar icon refresher.
Regression was introduced in 32c6b545d.

Telegram/SourceFiles/platform/win/main_window_win.cpp
@@ -646,7 +646,7 @@ int32 MainWindow::screenNameChecksum(const QString &name) const {
 }
 
 void MainWindow::psRefreshTaskbarIcon() {
-	auto refresher = std::unique_ptr<QWidget>(this);
+	auto refresher = std::make_unique<QWidget>(this);
 	refresher->setWindowFlags(static_cast<Qt::WindowFlags>(Qt::Tool) | Qt::FramelessWindowHint);
 	refresher->setGeometry(x() + 1, y() + 1, 1, 1);
 	auto palette = refresher->palette();