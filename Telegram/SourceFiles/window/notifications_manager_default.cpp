dmytrokurochkin/MZGram-Desktop

commit 9100dfc5550eda5aed0478a079f8675e90feb9cd

Remove BypassWindowManagerHint in MediaView / Passport.
It makes all kinds of usages buggy, including choose file dialog.

Fixes #4936.
I hope fixes #4950, I hope fixes #4955, I hope fixes #3553.

Telegram/SourceFiles/window/notifications_manager_default.cpp
@@ -360,7 +360,10 @@ Widget::Widget(Manager *manager, QPoint startPosition, int shift, Direction shif
 , _a_shift(animation(this, &Widget::step_shift)) {
 	setWindowOpacity(0.);
 
-	setWindowFlags(Qt::WindowFlags(Qt::FramelessWindowHint) | Qt::WindowStaysOnTopHint | Qt::BypassWindowManagerHint | Qt::NoDropShadowWindowHint | Qt::Tool);
+	setWindowFlags(Qt::WindowFlags(Qt::FramelessWindowHint)
+		| Qt::WindowStaysOnTopHint
+		| Qt::NoDropShadowWindowHint
+		| Qt::Tool);
 	setAttribute(Qt::WA_MacAlwaysShowToolWindow);
 	setAttribute(Qt::WA_OpaquePaintEvent);
 

Telegram/SourceFiles/boxes/peer_list_box.cpp
@@ -251,6 +251,7 @@ void PeerListController::setSearchNoResultsText(const QString &text) {
 }
 
 base::unique_qptr<Ui::PopupMenu> PeerListController::rowContextMenu(
+		QWidget *parent,
 		not_null<PeerListRow*> row) {
 	return nullptr;
 }
@@ -1020,7 +1021,7 @@ void PeerListContent::contextMenuEvent(QContextMenuEvent *e) {
 	}
 
 	if (const auto row = getRow(_contexted.index)) {
-		_contextMenu = _controller->rowContextMenu(row);
+		_contextMenu = _controller->rowContextMenu(this, row);
 		if (_contextMenu) {
 			_contextMenu->setDestroyedCallback(crl::guard(
 				this,

Telegram/SourceFiles/boxes/peer_list_box.h
@@ -345,6 +345,7 @@ public:
 	virtual void itemDeselectedHook(not_null<PeerData*> peer) {
 	}
 	virtual base::unique_qptr<Ui::PopupMenu> rowContextMenu(
+		QWidget *parent,
 		not_null<PeerListRow*> row);
 	bool isSearchLoading() const {
 		return _searchController ? _searchController->isLoading() : false;

Telegram/SourceFiles/calls/calls_panel.cpp
@@ -415,7 +415,7 @@ void Panel::reinitControls() {
 }
 
 void Panel::initLayout() {
-	setWindowFlags(Qt::WindowFlags(Qt::FramelessWindowHint) | Qt::WindowStaysOnTopHint | Qt::BypassWindowManagerHint | Qt::NoDropShadowWindowHint | Qt::Dialog);
+	setWindowFlags(Qt::WindowFlags(Qt::FramelessWindowHint) | Qt::WindowStaysOnTopHint | Qt::NoDropShadowWindowHint | Qt::Dialog);
 	setAttribute(Qt::WA_MacAlwaysShowToolWindow);
 	setAttribute(Qt::WA_NoSystemBackground, true);
 	setAttribute(Qt::WA_TranslucentBackground, true);

Telegram/SourceFiles/dialogs/dialogs_inner_widget.cpp
@@ -1514,7 +1514,7 @@ void DialogsInner::contextMenuEvent(QContextMenuEvent *e) {
 		mousePressReleased(_pressButton);
 	}
 
-	_menu = base::make_unique_q<Ui::PopupMenu>(nullptr);
+	_menu = base::make_unique_q<Ui::PopupMenu>(this);
 	if (const auto history = key.history()) {
 		Window::FillPeerMenu(
 			_controller,

Telegram/SourceFiles/history/admin_log/history_admin_log_inner.cpp
@@ -941,7 +941,7 @@ void InnerWidget::showContextMenu(QContextMenuEvent *e, bool showFromTouch) {
 		isUponSelected = hasSelected;
 	}
 
-	_menu = base::make_unique_q<Ui::PopupMenu>(nullptr);
+	_menu = base::make_unique_q<Ui::PopupMenu>(this);
 
 	const auto link = ClickHandler::getActive();
 	auto view = App::hoveredItem()

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -1456,7 +1456,7 @@ void HistoryInner::showContextMenu(QContextMenuEvent *e, bool showFromTouch) {
 		isUponSelected = hasSelected;
 	}
 
-	_menu = base::make_unique_q<Ui::PopupMenu>(nullptr);
+	_menu = base::make_unique_q<Ui::PopupMenu>(this);
 
 	const auto addItemActions = [&](HistoryItem *item) {
 		if (!item

Telegram/SourceFiles/history/view/history_view_context_menu.cpp
@@ -434,7 +434,7 @@ void AddCopyLinkAction(
 base::unique_qptr<Ui::PopupMenu> FillContextMenu(
 		not_null<ListWidget*> list,
 		const ContextMenuRequest &request) {
-	auto result = base::make_unique_q<Ui::PopupMenu>(nullptr);
+	auto result = base::make_unique_q<Ui::PopupMenu>(list);
 
 	const auto link = request.link;
 	const auto view = request.view;

commit 0220da13ef6b4ee099a55dc964db1842440c57c5

Bypass window manager for toast notifications.
Regression was introduced in 9100dfc555.

Fixes #5052.

Telegram/SourceFiles/window/notifications_manager_default.cpp
@@ -362,6 +362,7 @@ Widget::Widget(Manager *manager, QPoint startPosition, int shift, Direction shif
 
 	setWindowFlags(Qt::WindowFlags(Qt::FramelessWindowHint)
 		| Qt::WindowStaysOnTopHint
+		| Qt::BypassWindowManagerHint
 		| Qt::NoDropShadowWindowHint
 		| Qt::Tool);
 	setAttribute(Qt::WA_MacAlwaysShowToolWindow);