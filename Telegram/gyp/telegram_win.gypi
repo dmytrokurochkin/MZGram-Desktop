dmytrokurochkin/MZGram-Desktop

commit aa7a3f93cb207fbe5568cdf9d5820efba07983dc

Update OpenAL-Soft
Update to version 1.19.1

snap/snapcraft.yaml
@@ -284,7 +284,7 @@ parts:
   openal:
     source: https://github.com/kcat/openal-soft.git
     source-depth: 1
-    source-branch: v1.18
+    source-branch: v1.19
     plugin: cmake
     build-packages:
       - oss4-dev

.travis/build.sh
@@ -40,7 +40,7 @@ FFMPEG_PATH="$BUILD/ffmpeg"
 FFMPEG_CACHE_VERSION="3"
 
 OPENAL_PATH="$BUILD/openal-soft"
-OPENAL_CACHE_VERSION="3"
+OPENAL_CACHE_VERSION="4"
 
 GYP_DEFINES=""
 
@@ -476,6 +476,8 @@ buildOpenAL() {
 
   cd "$EXTERNAL"
   git clone https://github.com/kcat/openal-soft.git
+  cd openal-soft
+  git checkout openal-soft-1.19.1
 
   cd "$EXTERNAL/openal-soft/build"
   cmake \

commit 56c011801d50a441504eed81ae48d92825111426

Fix build after openal-soft update.
Regression was introduced in aa7a3f93cb.

Telegram/gyp/telegram_win.gypi
@@ -16,7 +16,6 @@
       '-lUxTheme',
       '-lDbgHelp',
       '-lOpenAL32',
-      '-lcommon',
       '-lopus',
       'windows/common',
       'windows/handler/exception_handler',

snap/snapcraft.yaml
@@ -284,7 +284,7 @@ parts:
   openal:
     source: https://github.com/kcat/openal-soft.git
     source-depth: 1
-    source-branch: v1.19
+    source-tag: openal-soft-1.19.1
     plugin: cmake
     build-packages:
       - oss4-dev