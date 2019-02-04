dmytrokurochkin/MZGram-Desktop

commit 7580bdac4da26f3cc5393173e363d8e660c817b8

Save wallpaper settings locally.

Telegram/SourceFiles/boxes/background_box.cpp
@@ -95,11 +95,11 @@ QImage PrepareScaledFromFull(
 		size);
 }
 
-QPixmap PrepareScaledFromThumb(not_null<Image*> thumb) {
+QPixmap PrepareScaledFromThumb(not_null<Image*> thumb, bool good) {
 	return thumb->loaded()
 		? App::pixmapFromImageInPlace(PrepareScaledFromFull(
 			thumb->original(),
-			Images::Option::Blurred))
+			good ? Images::Option(0) : Images::Option::Blurred))
 		: QPixmap();
 }
 
@@ -126,6 +126,11 @@ protected:
 
 private:
 	void updateWallpapers();
+	void paintPaper(
+		Painter &p,
+		const Data::WallPaper &paper,
+		int column,
+		int row) const;
 
 	Fn<void(int index)> _backgroundChosenCallback;
 
@@ -196,7 +201,7 @@ void BackgroundBox::Inner::updateWallpapers() {
 
 	const auto preload = kBackgroundsInRow * 3;
 	for (const auto &paper : papers | ranges::view::take(preload)) {
-		paper.thumb->load(Data::FileOrigin());
+		paper.loadThumbnail();
 	}
 }
 
@@ -223,24 +228,32 @@ void BackgroundBox::Inner::paintEvent(QPaintEvent *e) {
 		});
 		if ((st::backgroundSize.height() + st::backgroundPadding) * (row + 1) <= r.top()) {
 			continue;
+		} else if ((st::backgroundSize.height() + st::backgroundPadding) * row >= r.top() + r.height()) {
+			break;
 		}
+		paintPaper(p, paper, column, row);
+	}
+}
 
-		paper.thumb->load(Data::FileOrigin());
+void BackgroundBox::Inner::paintPaper(
+		Painter &p,
+		const Data::WallPaper &paper,
+		int column,
+		int row) const {
+	Expects(paper.thumbnail() != nullptr);
 
-		int x = st::backgroundPadding + column * (st::backgroundSize.width() + st::backgroundPadding);
-		int y = st::backgroundPadding + row * (st::backgroundSize.height() + st::backgroundPadding);
+	const auto x = st::backgroundPadding + column * (st::backgroundSize.width() + st::backgroundPadding);
+	const auto y = st::backgroundPadding + row * (st::backgroundSize.height() + st::backgroundPadding);
+	const auto &pixmap = paper.thumbnail()->pix(
+		paper.fileOrigin(),
+		st::backgroundSize.width(),
+		st::backgroundSize.height());
+	p.drawPixmap(x, y, pixmap);
 
-		const auto &pix = paper.thumb->pix(
-			Data::FileOrigin(),
-			st::backgroundSize.width(),
-			st::backgroundSize.height());
-		p.drawPixmap(x, y, pix);
-
-		if (paper.id == Window::Theme::Background()->id()) {
-			auto checkLeft = x + st::backgroundSize.width() - st::overviewCheckSkip - st::overviewCheck.size;
-			auto checkTop = y + st::backgroundSize.height() - st::overviewCheckSkip - st::overviewCheck.size;
-			_check->paint(p, getms(), checkLeft, checkTop, width());
-		}
+	if (paper.id() == Window::Theme::Background()->id()) {
+		const auto checkLeft = x + st::backgroundSize.width() - st::overviewCheckSkip - st::overviewCheck.size;
+		const auto checkTop = y + st::backgroundSize.height() - st::overviewCheckSkip - st::overviewCheck.size;
+		_check->paint(p, getms(), checkLeft, checkTop, width());
 	}
 }
 
@@ -300,6 +313,8 @@ BackgroundPreviewBox::BackgroundPreviewBox(
 	true))
 , _paper(paper)
 , _radial(animation(this, &BackgroundPreviewBox::step_radial)) {
+	Expects(_paper.thumbnail() != nullptr);
+
 	subscribe(Auth().downloaderTaskFinished(), [=] { update(); });
 }
 
@@ -308,26 +323,17 @@ void BackgroundPreviewBox::prepare() {
 
 	addButton(langFactory(lng_background_apply), [=] { apply(); });
 	addButton(langFactory(lng_cancel), [=] { closeBox(); });
-	if (!_paper.slug.isEmpty()) {
+	if (_paper.hasShareUrl()) {
 		addLeftButton(langFactory(lng_background_share), [=] { share(); });
 	}
 
-	_scaled = PrepareScaledFromThumb(_paper.thumb);
+	_paper.loadThumbnail();
+	_paper.loadDocument();
+	if (_paper.document() && _paper.document()->loading()) {
+		_radial.start(_paper.document()->progress());
+	}
+	setScaledFromThumb();
 	checkLoadedDocument();
-
-	if (!_paper.thumb->loaded()) {
-		_paper.thumb->loadEvenCancelled(Data::FileOriginWallpaper(
-			_paper.id,
-			_paper.accessHash));
-	}
-	if (_paper.document) {
-		_paper.document->save(Data::FileOriginWallpaper(
-			_paper.id,
-			_paper.accessHash), QString());
-		if (_paper.document->loading()) {
-			_radial.start(_paper.document->progress());
-		}
-	}
 
 	_text1->setDisplayDate(true);
 	_text1->initDimensions();
@@ -344,10 +350,7 @@ void BackgroundPreviewBox::apply() {
 }
 
 void BackgroundPreviewBox::share() {
-	Expects(!_paper.slug.isEmpty());
-
-	QApplication::clipboard()->setText(
-		Core::App().createInternalLinkFull("bg/" + _paper.slug));
+	QApplication::clipboard()->setText(_paper.shareUrl());
 	Ui::Toast::Show(lang(lng_background_link_copied));
 }
 
@@ -356,15 +359,12 @@ void BackgroundPreviewBox::paintEvent(QPaintEvent *e) {
 
 	const auto ms = getms();
 
-	if (const auto color = Window::Theme::GetWallPaperColor(_paper.slug)) {
+	if (const auto color = _paper.backgroundColor()) {
 		p.fillRect(e->rect(), *color);
 	} else {
-		if (_scaled.isNull()) {
-			_scaled = PrepareScaledFromThumb(_paper.thumb);
-			if (_scaled.isNull()) {
-				p.fillRect(e->rect(), st::boxBg);
-				return;
-			}
+		if (_scaled.isNull() && !setScaledFromThumb()) {
+			p.fillRect(e->rect(), st::boxBg);
+			return;
 		}
 		paintImage(p);
 		paintRadial(p, ms);
@@ -440,9 +440,9 @@ void BackgroundPreviewBox::paintTexts(Painter &p, TimeMs ms) {
 }
 
 void BackgroundPreviewBox::step_radial(TimeMs ms, bool timer) {
-	Expects(_paper.document != nullptr);
+	Expects(_paper.document() != nullptr);
 
-	const auto document = _paper.document;
+	const auto document = _paper.document();
 	const auto wasAnimating = _radial.animating();
 	const auto updated = _radial.update(
 		document->progress(),
@@ -456,8 +456,15 @@ void BackgroundPreviewBox::step_radial(TimeMs ms, bool timer) {
 	checkLoadedDocument();
 }
 
+bool BackgroundPreviewBox::setScaledFromThumb() {
+	_scaled = PrepareScaledFromThumb(
+		_paper.thumbnail(),
+		!_paper.document());
+	return !_scaled.isNull();
+}
+
 void BackgroundPreviewBox::checkLoadedDocument() {
-	const auto document = _paper.document;
+	const auto document = _paper.document();
 	if (!document
 		|| !document->loaded(DocumentData::FilePathResolveChecked)
 		|| _generating) {
@@ -491,13 +498,8 @@ void BackgroundPreviewBox::checkLoadedDocument() {
 }
 
 bool BackgroundPreviewBox::Start(const QString &slug, const QString &mode) {
-	if (Window::Theme::GetWallPaperColor(slug)) {
-		Ui::show(Box<BackgroundPreviewBox>(Data::WallPaper{
-			Window::Theme::kCustomBackground,
-			0ULL, // accessHash
-			MTPDwallPaper::Flags(0),
-			slug,
-		}));
+	if (const auto paper = Data::WallPaper::FromColorSlug(slug)) {
+		Ui::show(Box<BackgroundPreviewBox>(*paper));
 		return true;
 	}
 	if (!IsValidWallPaperSlug(slug)) {

Telegram/SourceFiles/apiwrap.cpp
@@ -884,24 +884,13 @@ void ApiWrap::requestWallPaper(
 	)).done([=](const MTPWallPaper &result) {
 		_wallPaperRequestId = 0;
 		_wallPaperSlug = QString();
-		result.match([&](const MTPDwallPaper &data) {
-			const auto document = _session->data().processDocument(
-				data.vdocument);
-			if (document->checkWallPaperProperties()) {
-				if (const auto done = base::take(_wallPaperDone)) {
-					done({
-						data.vid.v,
-						data.vaccess_hash.v,
-						data.vflags.v,
-						qs(data.vslug),
-						document->thumbnail(),
-						document
-					});
-				}
-			} else if (const auto fail = base::take(_wallPaperFail)) {
-				fail(RPCError::Local("BAD_DOCUMENT", "In a wallpaper."));
+		if (const auto paper = Data::WallPaper::Create(result)) {
+			if (const auto done = base::take(_wallPaperDone)) {
+				done(*paper);
 			}
-		});
+		} else if (const auto fail = base::take(_wallPaperFail)) {
+			fail(RPCError::Local("BAD_DOCUMENT", "In a wallpaper."));
+		}
 	}).fail([=](const RPCError &error) {
 		_wallPaperRequestId = 0;
 		_wallPaperSlug = QString();

Telegram/SourceFiles/apiwrap.h
@@ -25,7 +25,7 @@ class mtpFileLoader;
 
 namespace Data {
 struct UpdatedFileReferences;
-struct WallPaper;
+class WallPaper;
 } // namespace Data
 
 namespace InlineBots {

Telegram/SourceFiles/boxes/background_box.h
@@ -66,6 +66,7 @@ private:
 	QRect radialRect() const;
 
 	void checkLoadedDocument();
+	bool setScaledFromThumb();
 	void paintImage(Painter &p);
 	void paintRadial(Painter &p, TimeMs ms);
 	void paintTexts(Painter &p, TimeMs ms);

Telegram/SourceFiles/data/data_session.cpp
@@ -3070,41 +3070,23 @@ void Session::setWallpapers(const QVector<MTPWallPaper> &data, int32 hash) {
 		qsl(":/gui/art/bg.jpg"),
 		"JPG");
 	if (defaultBackground) {
-		_wallpapers.push_back({
-			Window::Theme::kDefaultBackground,
-			0ULL, // access_hash
-			MTPDwallPaper::Flags(0),
-			QString(), // slug
-			defaultBackground.get()
-		});
+		_wallpapers.push_back(Data::DefaultWallPaper());
+		_wallpapers.back().setLocalImageAsThumbnail(
+			defaultBackground.get());
 	}
 	const auto oldBackground = Images::Create(
 		qsl(":/gui/art/bg_initial.jpg"),
 		"JPG");
 	if (oldBackground) {
-		_wallpapers.push_back({
-			Window::Theme::kInitialBackground,
-			0ULL, // access_hash
-			MTPDwallPaper::Flags(0),
-			QString(), // slug
-			oldBackground.get()
-		});
+		_wallpapers.push_back(Data::Legacy1DefaultWallPaper());
+		_wallpapers.back().setLocalImageAsThumbnail(oldBackground.get());
 	}
 	for (const auto &paper : data) {
 		paper.match([&](const MTPDwallPaper &paper) {
 			if (paper.is_pattern()) {
 				return;
-			}
-			const auto document = processDocument(paper.vdocument);
-			if (document->checkWallPaperProperties()) {
-				_wallpapers.push_back({
-					paper.vid.v,
-					paper.vaccess_hash.v,
-					paper.vflags.v,
-					qs(paper.vslug),
-					document->thumbnail(),
-					document,
-				});
+			} else if (const auto parsed = Data::WallPaper::Create(paper)) {
+				_wallpapers.push_back(*parsed);
 			}
 		});
 	}

Telegram/SourceFiles/data/data_session.h
@@ -51,7 +51,7 @@ class Feed;
 enum class FeedUpdateFlag;
 struct FeedUpdate;
 
-struct WallPaper;
+class WallPaper;
 
 class Session final {
 public:

Telegram/SourceFiles/mainwidget.cpp
@@ -345,9 +345,16 @@ StackItemSection::StackItemSection(
 }
 
 struct MainWidget::SettingBackground {
+	explicit SettingBackground(const Data::WallPaper &data);
+
 	Data::WallPaper data;
 	base::binary_guard generating;
 };
+
+MainWidget::SettingBackground::SettingBackground(
+	const Data::WallPaper &data)
+: data(data) {
+}
 
 MainWidget::MainWidget(
 	QWidget *parent,
@@ -1554,16 +1561,11 @@ void MainWidget::setChatBackground(
 		return;
 	}
 
-	_background = std::make_unique<SettingBackground>();
-	_background->data = background;
-	_background->data.document->save(
-		Data::FileOriginWallpaper(
-			_background->data.id,
-			_background->data.accessHash),
-		QString());
+	_background = std::make_unique<SettingBackground>(background);
+	_background->data.loadDocument();
 	checkChatBackground();
 
-	const auto tile = (background.id == Window::Theme::kInitialBackground);
+	const auto tile = Data::IsLegacy1DefaultWallPaper(background);
 	using Update = Window::Theme::BackgroundUpdate;
 	Window::Theme::Background()->notify(Update(Update::Type::Start, tile));
 }
@@ -1571,9 +1573,7 @@ void MainWidget::setChatBackground(
 bool MainWidget::isReadyChatBackground(
 		const Data::WallPaper &background,
 		const QImage &image) const {
-	return !image.isNull()
-		|| !background.document
-		|| Window::Theme::GetWallPaperColor(background.slug);
+	return !image.isNull() || !background.document();
 }
 
 void MainWidget::setReadyChatBackground(
@@ -1582,22 +1582,22 @@ void MainWidget::setReadyChatBackground(
 	using namespace Window::Theme;
 
 	if (image.isNull()
-		&& !background.document
-		&& background.thumb
-		&& background.thumb->loaded()) {
-		image = background.thumb->pixNoCache(Data::FileOrigin()).toImage();
+		&& !background.document()
+		&& background.thumbnail()
+		&& background.thumbnail()->loaded()) {
+		image = background.thumbnail()->original();
 	}
 
 	const auto resetToDefault = image.isNull()
-		&& !background.document
-		&& !GetWallPaperColor(background.slug)
-		&& (background.id != kInitialBackground);
+		&& !background.document()
+		&& !background.backgroundColor()
+		&& !Data::IsLegacy1DefaultWallPaper(background);
 	const auto ready = resetToDefault
-		? Data::WallPaper{ kDefaultBackground }
+		? Data::DefaultWallPaper()
 		: background;
 
 	Background()->setImage(ready, std::move(image));
-	const auto tile = (ready.id == kInitialBackground);
+	const auto tile = Data::IsLegacy1DefaultWallPaper(ready);
 	Background()->setTile(tile);
 	Ui::ForceFullRepaint(this);
 }
@@ -1610,10 +1610,10 @@ float64 MainWidget::chatBackgroundProgress() const {
 	if (_background) {
 		if (_background->generating) {
 			return 1.;
-		} else if (_background->data.document) {
-			return _background->data.document->progress();
-		} else if (_background->data.thumb) {
-			return _background->data.thumb->progress();
+		} else if (const auto document = _background->data.document()) {
+			return document->progress();
+		} else if (const auto thumbnail = _background->data.thumbnail()) {
+			return thumbnail->progress();
 		}
 	}
 	return 1.;
@@ -1623,10 +1623,9 @@ void MainWidget::checkChatBackground() {
 	if (!_background || _background->generating) {
 		return;
 	}
-	const auto document = _background->data.document;
-	if (document && !document->loaded()) {
-		return;
-	} else if (!document && !_background->data.thumb->loaded()) {
+	const auto document = _background->data.document();
+	Assert(document != nullptr);
+	if (!document->loaded()) {
 		return;
 	}
 
@@ -1634,14 +1633,14 @@ void MainWidget::checkChatBackground() {
 			QImage &&image) {
 		const auto background = base::take(_background);
 		const auto ready = image.isNull()
-			? Data::WallPaper{ Window::Theme::kDefaultBackground }
+			? Data::DefaultWallPaper()
 			: background->data;
-		setChatBackground(ready, std::move(image));
+		setReadyChatBackground(ready, std::move(image));
 	});
 }
 
 Image *MainWidget::newBackgroundThumb() {
-	return _background ? _background->data.thumb : nullptr;
+	return _background ? _background->data.thumbnail() : nullptr;
 }
 
 void MainWidget::messageDataReceived(ChannelData *channel, MsgId msgId) {

Telegram/SourceFiles/mainwidget.h
@@ -27,7 +27,7 @@ struct PeerUpdate;
 } // namespace Notify
 
 namespace Data {
-struct WallPaper;
+class WallPaper;
 } // namespace Data
 
 namespace Dialogs {

commit 3a77fb28905da45731a0fe326d6909864f5e2522

Fix preview of color-only backgrounds.
Regression was introduced in 7580bdac4d.

Telegram/SourceFiles/boxes/background_box.cpp
@@ -368,8 +368,6 @@ BackgroundPreviewBox::BackgroundPreviewBox(
 	true))
 , _paper(paper)
 , _radial(animation(this, &BackgroundPreviewBox::step_radial)) {
-	Expects(_paper.thumbnail() != nullptr);
-
 	subscribe(Auth().downloaderTaskFinished(), [=] { update(); });
 }
 
@@ -540,10 +538,8 @@ void BackgroundPreviewBox::step_radial(TimeMs ms, bool timer) {
 }
 
 bool BackgroundPreviewBox::setScaledFromThumb() {
-	Expects(_paper.thumbnail() != nullptr);
-
 	const auto thumbnail = _paper.thumbnail();
-	if (!thumbnail->loaded()) {
+	if (!thumbnail || !thumbnail->loaded()) {
 		return false;
 	}
 	setScaledFromImage(PrepareScaledFromFull(