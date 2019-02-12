dmytrokurochkin/MZGram-Desktop

commit e92424fa50bcbd33af96b5cef19d9d6f88668f4a

Fix changelog dialogs list appearance.
Fixes #5678, fixes #5674.

Telegram/SourceFiles/history/history.cpp
@@ -2086,8 +2086,13 @@ void History::markFullyLoaded() {
 }
 
 void History::setLastMessage(HistoryItem *item) {
-	if (_lastMessage && *_lastMessage == item) {
-		return;
+	if (_lastMessage) {
+		if (*_lastMessage == item) {
+			return;
+		} else if (!IsServerMsgId((*_lastMessage)->id)
+				&& (*_lastMessage)->date() > item->date()) {
+			return;
+		}
 	}
 	_lastMessage = item;
 	_chatListMessage = std::nullopt;

commit e940634cd5e52a2db3c910dd98fafe72f84a1e2a

Fix another crash in last message checking.
Regression was introduced in e92424fa50.

Telegram/SourceFiles/history/history.cpp
@@ -2090,6 +2090,7 @@ void History::setLastMessage(HistoryItem *item) {
 		if (*_lastMessage == item) {
 			return;
 		} else if (*_lastMessage
+			&& item
 			&& !IsServerMsgId((*_lastMessage)->id)
 			&& (*_lastMessage)->date() > item->date()) {
 			return;