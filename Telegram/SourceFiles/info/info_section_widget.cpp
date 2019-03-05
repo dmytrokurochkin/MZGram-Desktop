dmytrokurochkin/MZGram-Desktop

commit bcede1065c910c3f2709005ab0d908fb93f2af9d

Optimize connecting widget.

Telegram/SourceFiles/info/info_section_widget.cpp
@@ -37,6 +37,8 @@ SectionWidget::SectionWidget(
 }
 
 void SectionWidget::init() {
+	Expects(_connecting == nullptr);
+
 	sizeValue(
 	) | rpl::start_with_next([wrap = _content.data()](QSize size) {
 		auto wrapGeometry = QRect{ { 0, 0 }, size };
@@ -44,14 +46,14 @@ void SectionWidget::init() {
 		wrap->updateGeometry(wrapGeometry, additionalScroll);
 	}, _content->lifetime());
 
-	_connecting = Window::ConnectingWidget::CreateDefaultWidget(
+	_connecting = std::make_unique<Window::ConnectionState>(
 		_content.data(),
 		Window::AdaptiveIsOneColumn());
 
 	_content->contentChanged(
 	) | rpl::start_with_next([=] {
 		_connecting->raise();
-	}, _connecting->lifetime());
+	}, _content->lifetime());
 }
 
 Dialogs::RowDescriptor SectionWidget::activeChat() const {

Telegram/SourceFiles/core/sandbox.cpp
@@ -137,10 +137,27 @@ int Sandbox::start() {
 }
 
 void Sandbox::launchApplication() {
-	if (_application) {
-		return;
-	}
+	InvokeQueued(this, [=] {
+		if (App::quitting()) {
+			quit();
+		} else if (_application) {
+			return;
+		}
+		setupScreenScale();
 
+		_application = std::make_unique<Application>(_launcher);
+
+		// Ideally this should go to constructor.
+		// But we want to catch all native events and Application installs
+		// its own filter that can filter out some of them. So we install
+		// our filter after the Application constructor installs his.
+		installNativeEventFilter(this);
+
+		_application->run();
+	});
+}
+
+void Sandbox::setupScreenScale() {
 	const auto dpi = Sandbox::primaryScreen()->logicalDotsPerInch();
 	LOG(("Primary screen DPI: %1").arg(dpi));
 	if (dpi <= 108) {
@@ -170,8 +187,6 @@ void Sandbox::launchApplication() {
 		cSetIntRetinaFactor(int32(ratio));
 		cSetScreenScale(kInterfaceScaleDefault);
 	}
-
-	runApplication();
 }
 
 Sandbox::~Sandbox() = default;
@@ -407,20 +422,6 @@ void Sandbox::checkForQuit() {
 	if (App::quitting()) {
 		quit();
 	}
-}
-
-void Sandbox::runApplication() {
-	Expects(!App::quitting());
-
-	_application = std::make_unique<Application>(_launcher);
-
-	// Ideally this should go to constructor.
-	// But we want to catch all native events and Application installs
-	// its own filter that can filter out some of them. So we install
-	// our filter after the Application constructor installs his.
-	installNativeEventFilter(this);
-
-	_application->run();
 }
 
 void Sandbox::refreshGlobalProxy() {

Telegram/SourceFiles/core/sandbox.h
@@ -81,7 +81,7 @@ private:
 	void processPostponedCalls(int level);
 	void singleInstanceChecked();
 	void launchApplication();
-	void runApplication();
+	void setupScreenScale();
 	void execExternal(const QString &cmd);
 
 	// Single instance application

Telegram/SourceFiles/dialogs/dialogs_widget.cpp
@@ -246,7 +246,7 @@ DialogsWidget::DialogsWidget(QWidget *parent, not_null<Window::Controller*> cont
 }
 
 void DialogsWidget::setupConnectingWidget() {
-	_connecting = Window::ConnectingWidget::CreateDefaultWidget(
+	_connecting = std::make_unique<Window::ConnectionState>(
 		this,
 		Window::AdaptiveIsOneColumn());
 }

Telegram/SourceFiles/dialogs/dialogs_widget.h
@@ -34,7 +34,7 @@ class FadeWrapScaled;
 
 namespace Window {
 class Controller;
-class ConnectingWidget;
+class ConnectionState;
 } // namespace Window
 
 enum DialogsSearchRequestType {
@@ -195,7 +195,7 @@ private:
 	class BottomButton;
 	object_ptr<BottomButton> _updateTelegram = { nullptr };
 	object_ptr<BottomButton> _loadMoreChats = { nullptr };
-	base::unique_qptr<Window::ConnectingWidget> _connecting;
+	std::unique_ptr<Window::ConnectionState> _connecting;
 
 	Animation _a_show;
 	Window::SlideDirection _showDirection;

Telegram/SourceFiles/info/info_section_widget.h
@@ -15,7 +15,7 @@ class SettingsSlider;
 } // namespace Ui
 
 namespace Window {
-class ConnectingWidget;
+class ConnectionState;
 } // namespace Window
 
 namespace Info {
@@ -69,7 +69,7 @@ private:
 
 	object_ptr<WrapWidget> _content;
 	object_ptr<Ui::RpWidget> _topBarSurrogate = { nullptr };
-	base::unique_qptr<Window::ConnectingWidget> _connecting;
+	std::unique_ptr<Window::ConnectionState> _connecting;
 
 };
 

Telegram/SourceFiles/intro/introwidget.cpp
@@ -114,7 +114,7 @@ Widget::Widget(QWidget *parent) : RpWidget(parent)
 }
 
 void Widget::setupConnectingWidget() {
-	_connecting = Window::ConnectingWidget::CreateDefaultWidget(
+	_connecting = std::make_unique<Window::ConnectionState>(
 		this,
 		rpl::single(true));
 }

Telegram/SourceFiles/intro/introwidget.h
@@ -24,7 +24,7 @@ class FadeWrap;
 } // namespace Ui
 
 namespace Window {
-class ConnectingWidget;
+class ConnectionState;
 } // namespace Window
 
 namespace Intro {
@@ -281,7 +281,7 @@ private:
 	object_ptr<Ui::FadeWrap<Ui::RoundButton>> _resetAccount = { nullptr };
 	object_ptr<Ui::FadeWrap<Ui::FlatLabel>> _terms = { nullptr };
 
-	base::unique_qptr<Window::ConnectingWidget> _connecting;
+	std::unique_ptr<Window::ConnectionState> _connecting;
 
 	mtpRequestId _resetRequest = 0;
 

commit 55d0ec5759a84517a851c445606f1693654a0c9e

Fixed crash in window resizing when layer is opened.
Regression was introduced in bcede1065c910c3f2709005ab0d908fb93f2af9d.

Telegram/SourceFiles/info/info_section_widget.cpp
@@ -53,7 +53,7 @@ void SectionWidget::init() {
 	_content->contentChanged(
 	) | rpl::start_with_next([=] {
 		_connecting->raise();
-	}, _content->lifetime());
+	}, _connecting->lifetime());
 }
 
 Dialogs::RowDescriptor SectionWidget::activeChat() const {