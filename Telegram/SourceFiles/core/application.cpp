dmytrokurochkin/MZGram-Desktop

commit c9d7fb38b1f7913ed755720e4a05858b0da2492e

Refactored checking of last input while notifications are displayed.  - Removed condition for Windows platform only.  - Added smooth hiding of notifications in case video is watched or voice message is recorded.  - psUserActionDone() was completely replaced with Core::App().updateNonIdle().

Telegram/SourceFiles/core/application.cpp
@@ -233,7 +233,7 @@ bool Application::eventFilter(QObject *object, QEvent *e) {
 	case QEvent::MouseButtonPress:
 	case QEvent::TouchBegin:
 	case QEvent::Wheel: {
-		psUserActionDone();
+		updateNonIdle();
 	} break;
 
 	case QEvent::ShortcutOverride: {
@@ -252,7 +252,7 @@ bool Application::eventFilter(QObject *object, QEvent *e) {
 
 	case QEvent::ApplicationActivate: {
 		if (object == QCoreApplication::instance()) {
-			psUserActionDone();
+			updateNonIdle();
 		}
 	} break;
 

Telegram/SourceFiles/platform/linux/specific_linux.cpp
@@ -193,16 +193,6 @@ void psDeleteDir(const QString &dir) {
 	_removeDirectory(dir);
 }
 
-namespace {
-
-auto _lastUserAction = 0LL;
-
-} // namespace
-
-void psUserActionDone() {
-	_lastUserAction = crl::now();
-}
-
 bool psIdleSupported() {
 	return false;
 }
@@ -446,7 +436,7 @@ bool OpenSystemSettings(SystemSettingsType type) {
 }
 
 crl::time LastUserInputTime() {
-	return _lastUserAction;
+	return 0LL;
 }
 
 namespace ThirdParty {

Telegram/SourceFiles/platform/linux/specific_linux.h
@@ -51,7 +51,6 @@ void psWriteDump();
 
 void psDeleteDir(const QString &dir);
 
-void psUserActionDone();
 bool psIdleSupported();
 
 QStringList psInitLogs();

Telegram/SourceFiles/platform/mac/specific_mac.h
@@ -52,7 +52,6 @@ void psWriteDump();
 
 void psDeleteDir(const QString &dir);
 
-void psUserActionDone();
 bool psIdleSupported();
 
 QStringList psInitLogs();

Telegram/SourceFiles/platform/mac/specific_mac.mm
@@ -90,16 +90,6 @@ void psDeleteDir(const QString &dir) {
 	objc_deleteDir(dir);
 }
 
-namespace {
-
-auto _lastUserAction = 0LL;
-
-} // namespace
-
-void psUserActionDone() {
-	_lastUserAction = crl::now();
-}
-
 bool psIdleSupported() {
 	return objc_idleSupported();
 }
@@ -267,7 +257,7 @@ bool OpenSystemSettings(SystemSettingsType type) {
 
 crl::time LastUserInputTime() {
 	auto idleTime = 0LL;
-	return objc_idleTime(idleTime) ? (crl::now() - crl::time(idleTime)) : _lastUserAction;
+	return objc_idleTime(idleTime) ? (crl::now() - crl::time(idleTime)) : 0LL;
 }
 
 } // namespace Platform

Telegram/SourceFiles/platform/win/specific_win.cpp
@@ -128,17 +128,6 @@ namespace {
 	}
 }
 
-namespace {
-
-crl::time _lastUserAction = 0;
-
-} // namespace
-
-void psUserActionDone() {
-	_lastUserAction = crl::now();
-	EventFilter::getInstance()->setSessionLoggedOff(false);
-}
-
 bool psIdleSupported() {
 	LASTINPUTINFO lii;
 	lii.cbSize = sizeof(LASTINPUTINFO);
@@ -347,7 +336,7 @@ QString CurrentExecutablePath(int argc, char *argv[]) {
 crl::time LastUserInputTime() {
 	LASTINPUTINFO lii;
 	lii.cbSize = sizeof(LASTINPUTINFO);
-	return GetLastInputInfo(&lii) ? (crl::now() + lii.dwTime - GetTickCount()) : _lastUserAction;
+	return GetLastInputInfo(&lii) ? (crl::now() + lii.dwTime - GetTickCount()) : 0LL;
 }
 
 namespace {

Telegram/SourceFiles/platform/win/specific_win.h
@@ -59,7 +59,6 @@ void psWriteDump();
 
 void psDeleteDir(const QString &dir);
 
-void psUserActionDone();
 bool psIdleSupported();
 
 QStringList psInitLogs();

Telegram/SourceFiles/window/notifications_manager_default.cpp
@@ -505,9 +505,7 @@ Notification::Notification(
 	int shift,
 	Direction shiftDirection)
 : Widget(manager, startPosition, shift, shiftDirection)
-#ifdef Q_OS_WIN
-, _started(GetTickCount())
-#endif // Q_OS_WIN
+, _started(crl::now())
 , _history(history)
 , _peer(peer)
 , _author(author)
@@ -588,14 +586,7 @@ void Notification::prepareActionsCache() {
 bool Notification::checkLastInput(bool hasReplyingNotifications) {
 	if (!_waitingForInput) return true;
 
-	auto wasUserInput = true; // TODO
-#ifdef Q_OS_WIN
-	LASTINPUTINFO lii;
-	lii.cbSize = sizeof(LASTINPUTINFO);
-	BOOL res = GetLastInputInfo(&lii);
-	wasUserInput = (!res || lii.dwTime >= _started);
-#endif // Q_OS_WIN
-	if (wasUserInput) {
+	if (Core::App().lastNonIdleTime() > _started) {
 		_waitingForInput = false;
 		if (!hasReplyingNotifications) {
 			_hideTimer.start(st::notifyWaitLongHide);

commit af8391cff5593545aed951fe98f5d34ce49fccf7

Fix notifications hiding on Linux.
Regression was introduced in c9d7fb38b1.

Only when platform-specific code can give us the global time of the
last user input event we rely on idle time for notifications hiding.

Fixes #5791.

Telegram/SourceFiles/core/application.cpp
@@ -860,7 +860,9 @@ void Application::updateNonIdle() {
 }
 
 crl::time Application::lastNonIdleTime() const {
-	return std::max(Platform::LastUserInputTime(), _lastNonIdleTime);
+	return std::max(
+		Platform::LastUserInputTime().value_or(0),
+		_lastNonIdleTime);
 }
 
 rpl::producer<bool> Application::passcodeLockChanges() const {

Telegram/SourceFiles/platform/linux/specific_linux.cpp
@@ -193,10 +193,6 @@ void psDeleteDir(const QString &dir) {
 	_removeDirectory(dir);
 }
 
-bool psIdleSupported() {
-	return false;
-}
-
 void psActivateProcess(uint64 pid) {
 //	objc_activateProgram();
 }
@@ -433,10 +429,6 @@ bool OpenSystemSettings(SystemSettingsType type) {
 		}) != end(options);
 	}
 	return true;
-}
-
-crl::time LastUserInputTime() {
-	return 0LL;
 }
 
 namespace ThirdParty {

Telegram/SourceFiles/platform/linux/specific_linux.h
@@ -33,7 +33,9 @@ inline void ReInitOnTopPanel(QWidget *panel) {
 
 QString CurrentExecutablePath(int argc, char *argv[]);
 
-crl::time LastUserInputTime();
+inline std::optional<crl::time> LastUserInputTime() {
+	return std::nullopt;
+}
 
 } // namespace Platform
 
@@ -50,8 +52,6 @@ inline void psCheckLocalSocket(const QString &serverName) {
 void psWriteDump();
 
 void psDeleteDir(const QString &dir);
-
-bool psIdleSupported();
 
 QStringList psInitLogs();
 void psClearInitLogs();

Telegram/SourceFiles/platform/mac/specific_mac.h
@@ -19,8 +19,6 @@ inline bool TranslucentWindowsSupported(QPoint globalPosition) {
 
 QString CurrentExecutablePath(int argc, char *argv[]);
 
-crl::time LastUserInputTime();
-
 void RemoveQuarantine(const QString &path);
 
 namespace ThirdParty {
@@ -51,8 +49,6 @@ inline void psCheckLocalSocket(const QString &serverName) {
 void psWriteDump();
 
 void psDeleteDir(const QString &dir);
-
-bool psIdleSupported();
 
 QStringList psInitLogs();
 void psClearInitLogs();

Telegram/SourceFiles/platform/mac/specific_mac.mm
@@ -90,10 +90,6 @@ void psDeleteDir(const QString &dir) {
 	objc_deleteDir(dir);
 }
 
-bool psIdleSupported() {
-	return objc_idleSupported();
-}
-
 QStringList psInitLogs() {
 	return _initLogs;
 }
@@ -255,9 +251,58 @@ bool OpenSystemSettings(SystemSettingsType type) {
 	return true;
 }
 
-crl::time LastUserInputTime() {
-	auto idleTime = 0LL;
-	return objc_idleTime(idleTime) ? (crl::now() - crl::time(idleTime)) : 0LL;
+// Taken from https://github.com/trueinteractions/tint/issues/53.
+std::optional<crl::time> LastUserInputTime() {
+	CFMutableDictionaryRef properties = 0;
+	CFTypeRef obj;
+	mach_port_t masterPort;
+	io_iterator_t iter;
+	io_registry_entry_t curObj;
+
+	IOMasterPort(MACH_PORT_NULL, &masterPort);
+
+	/* Get IOHIDSystem */
+	IOServiceGetMatchingServices(masterPort, IOServiceMatching("IOHIDSystem"), &iter);
+	if (iter == 0) {
+		return std::nullopt;
+	} else {
+		curObj = IOIteratorNext(iter);
+	}
+	if (IORegistryEntryCreateCFProperties(curObj, &properties, kCFAllocatorDefault, 0) == KERN_SUCCESS && properties != NULL) {
+		obj = CFDictionaryGetValue(properties, CFSTR("HIDIdleTime"));
+		CFRetain(obj);
+	} else {
+		return std::nullopt;
+	}
+
+	uint64 err = ~0L, idleTime = err;
+	if (obj) {
+		CFTypeID type = CFGetTypeID(obj);
+
+		if (type == CFDataGetTypeID()) {
+			CFDataGetBytes((CFDataRef) obj, CFRangeMake(0, sizeof(idleTime)), (UInt8*)&idleTime);
+		} else if (type == CFNumberGetTypeID()) {
+			CFNumberGetValue((CFNumberRef)obj, kCFNumberSInt64Type, &idleTime);
+		} else {
+			// error
+		}
+
+		CFRelease(obj);
+
+		if (idleTime != err) {
+			idleTime /= 1000000; // return as ms
+		}
+	} else {
+		// error
+	}
+
+	CFRelease((CFTypeRef)properties);
+	IOObjectRelease(curObj);
+	IOObjectRelease(iter);
+	if (idleTime == err) {
+		return std::nullopt;
+	}
+	return (crl::now() - static_cast<crl::time>(idleTime));
 }
 
 } // namespace Platform

Telegram/SourceFiles/platform/mac/specific_mac_p.h
@@ -16,8 +16,6 @@ void objc_bringToBack(WId winId);
 
 void objc_debugShowAlert(const QString &str);
 void objc_outputDebugString(const QString &str);
-bool objc_idleSupported();
-bool objc_idleTime(crl::time &idleTime);
 
 void objc_start();
 void objc_ignoreApplicationActivationRightNow();

Telegram/SourceFiles/platform/mac/specific_mac_p.mm
@@ -343,64 +343,6 @@ void objc_outputDebugString(const QString &str) {
 	}
 }
 
-bool objc_idleSupported() {
-	auto idleTime = 0LL;
-	return objc_idleTime(idleTime);
-}
-
-bool objc_idleTime(crl::time &idleTime) { // taken from https://github.com/trueinteractions/tint/issues/53
-	CFMutableDictionaryRef properties = 0;
-	CFTypeRef obj;
-	mach_port_t masterPort;
-	io_iterator_t iter;
-	io_registry_entry_t curObj;
-
-	IOMasterPort(MACH_PORT_NULL, &masterPort);
-
-	/* Get IOHIDSystem */
-	IOServiceGetMatchingServices(masterPort, IOServiceMatching("IOHIDSystem"), &iter);
-	if (iter == 0) {
-		return false;
-	} else {
-		curObj = IOIteratorNext(iter);
-	}
-	if (IORegistryEntryCreateCFProperties(curObj, &properties, kCFAllocatorDefault, 0) == KERN_SUCCESS && properties != NULL) {
-		obj = CFDictionaryGetValue(properties, CFSTR("HIDIdleTime"));
-		CFRetain(obj);
-	} else {
-		return false;
-	}
-
-	uint64 err = ~0L, result = err;
-	if (obj) {
-		CFTypeID type = CFGetTypeID(obj);
-
-		if (type == CFDataGetTypeID()) {
-			CFDataGetBytes((CFDataRef) obj, CFRangeMake(0, sizeof(result)), (UInt8*)&result);
-		} else if (type == CFNumberGetTypeID()) {
-			CFNumberGetValue((CFNumberRef)obj, kCFNumberSInt64Type, &result);
-		} else {
-			// error
-		}
-
-		CFRelease(obj);
-
-		if (result != err) {
-			result /= 1000000; // return as ms
-		}
-	} else {
-		// error
-	}
-
-	CFRelease((CFTypeRef)properties);
-	IOObjectRelease(curObj);
-	IOObjectRelease(iter);
-	if (result == err) return false;
-
-	idleTime = static_cast<crl::time>(result);
-	return true;
-}
-
 void objc_start() {
 	_sharedDelegate = [[ApplicationDelegate alloc] init];
 	[[NSApplication sharedApplication] setDelegate:_sharedDelegate];

Telegram/SourceFiles/platform/platform_specific.h
@@ -40,8 +40,13 @@ void RequestPermission(PermissionType type, Fn<void(PermissionStatus)> resultCal
 void OpenSystemSettingsForPermission(PermissionType type);
 bool OpenSystemSettings(SystemSettingsType type);
 
-QString SystemLanguage();
-QString SystemCountry();
+[[nodiscard]] QString SystemLanguage();
+[[nodiscard]] QString SystemCountry();
+
+[[nodiscard]] std::optional<crl::time> LastUserInputTime();
+[[nodiscard]] inline bool LastUserInputTimeSupported() {
+	return LastUserInputTime().has_value();
+}
 
 namespace ThirdParty {
 

Telegram/SourceFiles/platform/win/specific_win.cpp
@@ -128,12 +128,6 @@ namespace {
 	}
 }
 
-bool psIdleSupported() {
-	LASTINPUTINFO lii;
-	lii.cbSize = sizeof(LASTINPUTINFO);
-	return GetLastInputInfo(&lii);
-}
-
 QStringList psInitLogs() {
     return _initLogs;
 }
@@ -333,10 +327,12 @@ QString CurrentExecutablePath(int argc, char *argv[]) {
 	return QString();
 }
 
-crl::time LastUserInputTime() {
-	LASTINPUTINFO lii;
+std::optional<crl::time> LastUserInputTime() {
+	auto lii = LASTINPUTINFO{ 0 };
 	lii.cbSize = sizeof(LASTINPUTINFO);
-	return GetLastInputInfo(&lii) ? (crl::now() + lii.dwTime - GetTickCount()) : 0LL;
+	return GetLastInputInfo(&lii)
+		? std::make_optional(crl::now() + lii.dwTime - GetTickCount())
+		: std::nullopt;
 }
 
 namespace {

Telegram/SourceFiles/platform/win/specific_win.h
@@ -36,8 +36,6 @@ inline void ReInitOnTopPanel(QWidget *panel) {
 
 QString CurrentExecutablePath(int argc, char *argv[]);
 
-crl::time LastUserInputTime();
-
 namespace ThirdParty {
 
 inline void start() {
@@ -58,8 +56,6 @@ inline void psCheckLocalSocket(const QString &) {
 void psWriteDump();
 
 void psDeleteDir(const QString &dir);
-
-bool psIdleSupported();
 
 QStringList psInitLogs();
 void psClearInitLogs();

Telegram/SourceFiles/settings/settings_privacy_security.cpp
@@ -205,7 +205,7 @@ void SetupLocalPasscode(not_null<Ui::VerticalLayout*> container) {
 		Ui::show(Box<PasscodeBox>(true));
 	});
 
-	const auto label = psIdleSupported()
+	const auto label = Platform::LastUserInputTimeSupported()
 		? lng_passcode_autolock_away
 		: lng_passcode_autolock_inactive;
 	auto value = PasscodeChanges(

Telegram/SourceFiles/window/notifications_manager_default.cpp
@@ -586,7 +586,10 @@ void Notification::prepareActionsCache() {
 bool Notification::checkLastInput(bool hasReplyingNotifications) {
 	if (!_waitingForInput) return true;
 
-	if (Core::App().lastNonIdleTime() > _started) {
+	const auto waitForUserInput = Platform::LastUserInputTimeSupported()
+		? (Core::App().lastNonIdleTime() <= _started)
+		: false;
+	if (!waitForUserInput) {
 		_waitingForInput = false;
 		if (!hasReplyingNotifications) {
 			_hideTimer.start(st::notifyWaitLongHide);