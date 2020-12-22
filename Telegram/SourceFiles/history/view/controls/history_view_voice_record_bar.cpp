dmytrokurochkin/MZGram-Desktop

commit c23eb576a4ee08faeb8a479e37843f416621808f

Fixed ability to reply with voice message.

Telegram/SourceFiles/history/view/controls/history_view_voice_record_bar.cpp
@@ -1307,6 +1307,7 @@ void VoiceRecordBar::hideFast() {
 void VoiceRecordBar::stopRecording(StopType type) {
 	using namespace ::Media::Capture;
 	if (type == StopType::Cancel) {
+		_cancelRequests.fire({});
 		instance()->stop();
 		return;
 	}
@@ -1402,6 +1403,10 @@ rpl::producer<SendActionUpdate> VoiceRecordBar::sendActionUpdates() const {
 
 rpl::producer<VoiceToSend> VoiceRecordBar::sendVoiceRequests() const {
 	return _sendVoiceRequests.events();
+}
+
+rpl::producer<> VoiceRecordBar::cancelRequests() const {
+	return _cancelRequests.events();
 }
 
 bool VoiceRecordBar::isRecording() const {

Telegram/SourceFiles/history/history_widget.cpp
@@ -773,6 +773,12 @@ void HistoryWidget::initVoiceRecordBar() {
 		return false;
 	});
 
+	const auto applyLocalDraft = [=] {
+		if (_history && _history->localDraft()) {
+			applyDraft();
+		}
+	};
+
 	_voiceRecordBar->sendActionUpdates(
 	) | rpl::start_with_next([=](const auto &data) {
 		if (!_history) {
@@ -799,7 +805,11 @@ void HistoryWidget::initVoiceRecordBar() {
 			data.duration,
 			action);
 		_voiceRecordBar->clearListenState();
+		applyLocalDraft();
 	}, lifetime());
+
+	_voiceRecordBar->cancelRequests(
+	) | rpl::start_with_next(applyLocalDraft, lifetime());
 
 	_voiceRecordBar->lockShowStarts(
 	) | rpl::start_with_next([=] {
@@ -815,13 +825,6 @@ void HistoryWidget::initVoiceRecordBar() {
 	_voiceRecordBar->lockViewportEvents(
 	) | rpl::start_with_next([=](not_null<QEvent*> e) {
 		_scroll->viewportEvent(e);
-	}, lifetime());
-
-	_voiceRecordBar->shownValue(
-	) | rpl::start_with_next([=](bool shown) {
-		if (!shown) {
-			applyDraft();
-		}
 	}, lifetime());
 
 	_voiceRecordBar->hideFast();
@@ -1620,13 +1623,16 @@ void HistoryWidget::fastShowAtEnd(not_null<History*> history) {
 void HistoryWidget::applyDraft(FieldHistoryAction fieldHistoryAction) {
 	InvokeQueued(this, [=] { updateStickersByEmoji(); });
 
+	if (_voiceRecordBar->isActive()) {
+		return;
+	}
+
 	auto draft = !_history
 		? nullptr
 		: _history->localEditDraft()
 		? _history->localEditDraft()
 		: _history->localDraft();
-	auto fieldAvailable = canWriteMessage()
-		&& !_voiceRecordBar->isActive();
+	auto fieldAvailable = canWriteMessage();
 	if (!draft || (!_history->localEditDraft() && !fieldAvailable)) {
 		auto fieldWillBeHiddenAfterEdit = (!fieldAvailable && _editMsgId != 0);
 		clearFieldText(0, fieldHistoryAction);

Telegram/SourceFiles/history/view/controls/history_view_voice_record_bar.h
@@ -61,6 +61,7 @@ public:
 
 	[[nodiscard]] rpl::producer<SendActionUpdate> sendActionUpdates() const;
 	[[nodiscard]] rpl::producer<VoiceToSend> sendVoiceRequests() const;
+	[[nodiscard]] rpl::producer<> cancelRequests() const;
 	[[nodiscard]] rpl::producer<bool> recordingStateChanges() const;
 	[[nodiscard]] rpl::producer<bool> lockShowStarts() const;
 	[[nodiscard]] rpl::producer<not_null<QEvent*>> lockViewportEvents() const;
@@ -128,6 +129,7 @@ private:
 
 	rpl::event_stream<SendActionUpdate> _sendActionUpdates;
 	rpl::event_stream<VoiceToSend> _sendVoiceRequests;
+	rpl::event_stream<> _cancelRequests;
 	rpl::event_stream<> _listenChanges;
 
 	int _centerY = 0;

commit 6b76d6e72cede16369f606fa4c19e4f6a040bfe3

Fixed stack overflow crash in applying draft at end of voice recording.
Regression was introduced in c23eb576a4.

Telegram/SourceFiles/history/view/controls/history_view_voice_record_bar.cpp
@@ -1389,8 +1389,9 @@ void VoiceRecordBar::hideFast() {
 void VoiceRecordBar::stopRecording(StopType type) {
 	using namespace ::Media::Capture;
 	if (type == StopType::Cancel) {
-		_cancelRequests.fire({});
-		instance()->stop();
+		instance()->stop(crl::guard(this, [=](Result &&data) {
+			_cancelRequests.fire({});
+		}));
 		return;
 	}
 	instance()->stop(crl::guard(this, [=](Result &&data) {