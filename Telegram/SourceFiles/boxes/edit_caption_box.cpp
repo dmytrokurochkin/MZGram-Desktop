dmytrokurochkin/MZGram-Desktop

commit 8181407d60a659b9f253ab02c287a7bb9857c727

Completely refactored EditCaptionBox.
Moved preview content to separate widget.
Increased caption area height.

Telegram/SourceFiles/boxes/edit_caption_box.h
@@ -8,11 +8,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #pragma once
 
 #include "boxes/abstract_box.h"
-#include "storage/storage_media_prepare.h"
-#include "ui/wrap/slide_wrap.h"
 #include "ui/chat/attach/attach_prepare.h"
-
-class Image;
 
 namespace ChatHelpers {
 class TabbedPanel;
@@ -23,36 +19,19 @@ class SessionController;
 } // namespace Window
 
 namespace Data {
-class Media;
 class PhotoMedia;
-class DocumentMedia;
 } // namespace Data
 
 namespace Ui {
-class AbstractButton;
+class AbstractSinglePreview;
 class InputField;
 class EmojiButton;
-class IconButton;
-class Checkbox;
+class VerticalLayout;
+class FadeShadow;
 enum class AlbumType;
-class AttachControlsWidget;
 } // namespace Ui
 
-namespace Window {
-class SessionController;
-} // namespace Window
-
-namespace Media {
-namespace Streaming {
-class Instance;
-class Document;
-struct Update;
-enum class Error;
-struct Information;
-} // namespace Streaming
-} // namespace Media
-
-class EditCaptionBox final : public Ui::BoxContent, private base::Subscriber {
+class EditCaptionBox final : public Ui::BoxContent {
 public:
 	EditCaptionBox(
 		QWidget*,
@@ -69,15 +48,15 @@ protected:
 	void keyPressEvent(QKeyEvent *e) override;
 
 private:
+	void rebuildPreview();
+	void setupEditEventHandler();
+	void setupPhotoEditorEventHandler();
+	void setupShadows();
+	void setupField();
+	void setupControls();
+
 	void updateBoxSize();
-	void prepareStreamedPreview();
-	void checkStreamedIsStarted();
-	void setupStreamedPreview(
-		std::shared_ptr<::Media::Streaming::Document> shared);
-	void handleStreamingUpdate(::Media::Streaming::Update &&update);
-	void handleStreamingError(::Media::Streaming::Error &&error);
-	void streamingReady(::Media::Streaming::Information &&info);
-	void startStreamedPlayer();
+	void captionResized();
 
 	void setupEmojiPanel();
 	void updateEmojiPanelGeometry();
@@ -86,74 +65,43 @@ private:
 	void setupDragArea();
 
 	void save();
-	void captionResized();
 
-	void setName(QString nameString, qint64 size);
 	bool fileFromClipboard(not_null<const QMimeData*> data);
-	void updateEditPreview();
-	void updateEditMediaButton();
-	void updateCaptionMaxHeight();
 
 	int errorTopSkip() const;
 
-	void createEditMediaButton();
 	bool setPreparedList(Ui::PreparedList &&list);
 
-	bool isThumbedLayout() const;
-
-	inline QString getNewMediaPath() {
-		return _preparedList.files.empty()
-			? QString()
-			: _preparedList.files.front().path;
-	}
-
 	const not_null<Window::SessionController*> _controller;
+	const not_null<HistoryItem*> _historyItem;
+	const bool _isAllowedEditMedia = false;
+	const Ui::AlbumType _albumType;
 
-	FullMsgId _msgId;
-	std::shared_ptr<Data::PhotoMedia> _photoMedia;
-	std::shared_ptr<Data::DocumentMedia> _documentMedia;
-	bool _thumbnailImageLoaded = false;
-	Fn<void()> _refreshThumbnail;
-	bool _animated = false;
-	bool _photo = false;
-	bool _doc = false;
+	const base::unique_qptr<Ui::VerticalLayout> _controls;
+	const base::unique_qptr<Ui::ScrollArea> _scroll;
+	const base::unique_qptr<Ui::InputField> _field;
+	const base::unique_qptr<Ui::EmojiButton> _emojiToggle;
+	const base::unique_qptr<Ui::FadeShadow> _topShadow,_bottomShadow;
 
-	QPixmap _thumb;
-	std::unique_ptr<::Media::Streaming::Instance> _streamed;
-
-	object_ptr<Ui::InputField> _field = { nullptr };
-	object_ptr<Ui::EmojiButton> _emojiToggle = { nullptr };
+	base::unique_qptr<Ui::AbstractSinglePreview> _content;
 	base::unique_qptr<ChatHelpers::TabbedPanel> _emojiPanel;
 	base::unique_qptr<QObject> _emojiFilter;
-	base::unique_qptr<Ui::AbstractButton> _photoEditorButton;
 
-	int _thumbx = 0;
-	int _thumbw = 0;
-	int _thumbh = 0;
-	Ui::Text::String _name;
-	QString _status;
-	bool _isAudio = false;
-	bool _isImage = false;
-
-	int _gifw = 0;
-	int _gifh = 0;
-	int _gifx = 0;
+	std::shared_ptr<Data::PhotoMedia> _photoMedia;
 
 	Ui::PreparedList _preparedList;
 
 	mtpRequestId _saveRequestId = 0;
 
-	object_ptr<Ui::AttachControlsWidget> _editMedia = nullptr;
-	object_ptr<Ui::IconButton> _editFile = nullptr;
-	Ui::SlideWrap<Ui::RpWidget> *_wayWrap = nullptr;
-	Ui::SlideWrap<Ui::RpWidget> *_hintLabel = nullptr;
-	QString _newMediaPath;
-	Ui::AlbumType _albumType = Ui::AlbumType();
-	bool _isAllowedEditMedia = false;
 	bool _asFile = false;
-	rpl::event_stream<> _editMediaClicks;
-	rpl::event_stream<> _photoEditorOpens;
 
 	QString _error;
+
+	rpl::variable<bool> _isPhoto = false;
+	rpl::variable<int> _footerHeight = 0;
+
+	rpl::event_stream<> _editMediaClicks;
+	rpl::event_stream<> _photoEditorOpens;
+	rpl::event_stream<int> _contentHeight;
 
 };

commit 9b16fa77e834bb73692ef72468ba24451112964e

Fixed some bugs in EditCaptionBox.
Fixed checkbox display in some cases.
Fixed editing of album items.

Regression was introduced in 8181407d60.

Telegram/SourceFiles/boxes/edit_caption_box.cpp
@@ -101,6 +101,11 @@ Ui::AlbumType ComputeAlbumType(not_null<HistoryItem*> item) {
 	return Ui::AlbumType();
 }
 
+bool CanBeCompressed(Ui::AlbumType type) {
+	return (type == Ui::AlbumType::None)
+		|| (type == Ui::AlbumType::PhotoVideo);
+}
+
 } // namespace
 
 EditCaptionBox::EditCaptionBox(
@@ -170,7 +175,7 @@ void EditCaptionBox::rebuildPreview() {
 		const auto photo = media->photo();
 		const auto document = media->document();
 		if (photo || document->isVideoFile() || document->isAnimation()) {
-			_isPhoto = true;
+			_isPhoto = (photo != nullptr);
 			const auto media = Ui::CreateChild<Ui::ItemSingleMediaPreview>(
 				this,
 				gifPaused,
@@ -229,6 +234,8 @@ void EditCaptionBox::rebuildPreview() {
 
 	_scroll->setOwnedWidget(
 		object_ptr<Ui::RpWidget>::fromRaw(_content.get()));
+
+	_previewRebuilds.fire({});
 
 	captionResized();
 }
@@ -302,10 +309,11 @@ void EditCaptionBox::setupShadows() {
 }
 
 void EditCaptionBox::setupControls() {
-	auto hintLabelToggleOn = _isPhoto.value(
-	) | rpl::map([=](bool value) {
+	auto hintLabelToggleOn = _previewRebuilds.events_starting_with(
+		rpl::empty_value()
+	) | rpl::map([=] {
 		return _controller->session().settings().photoEditorHintShown()
-			? value
+			? _isPhoto
 			: false;
 	});
 
@@ -327,9 +335,12 @@ void EditCaptionBox::setupControls() {
 			st::defaultBoxCheckbox),
 		st::editMediaCheckboxMargins)
 	)->toggleOn(
-		_isPhoto.value(
-		) | rpl::map([=](bool value) {
-			return value && (_albumType == Ui::AlbumType::None);
+		_previewRebuilds.events_starting_with(
+			rpl::empty_value()
+		) | rpl::map([=] {
+			return _isPhoto
+				&& CanBeCompressed(_albumType)
+				&& !_preparedList.files.empty();
 		}),
 		anim::type::instant
 	)->entity()->checkedChanges(
@@ -610,6 +621,7 @@ void EditCaptionBox::resizeEvent(QResizeEvent *e) {
 	_emojiToggle->update();
 
 	if (!_controls->isHidden()) {
+		_controls->resizeToWidth(width());
 		_controls->moveToLeft(
 			st::boxPhotoPadding.left(),
 			bottom - _controls->heightNoMargins());
@@ -661,7 +673,7 @@ void EditCaptionBox::save() {
 
 		_controller->session().api().editMedia(
 			std::move(_preparedList),
-			(!_asFile && _isPhoto.current())
+			(!_asFile && _isPhoto && CanBeCompressed(_albumType))
 				? SendMediaType::Photo
 				: SendMediaType::File,
 			_field->getTextWithAppliedMarkdown(),

Telegram/SourceFiles/boxes/edit_caption_box.h
@@ -74,14 +74,14 @@ private:
 
 	const not_null<Window::SessionController*> _controller;
 	const not_null<HistoryItem*> _historyItem;
-	const bool _isAllowedEditMedia = false;
+	const bool _isAllowedEditMedia;
 	const Ui::AlbumType _albumType;
 
 	const base::unique_qptr<Ui::VerticalLayout> _controls;
 	const base::unique_qptr<Ui::ScrollArea> _scroll;
 	const base::unique_qptr<Ui::InputField> _field;
 	const base::unique_qptr<Ui::EmojiButton> _emojiToggle;
-	const base::unique_qptr<Ui::FadeShadow> _topShadow,_bottomShadow;
+	const base::unique_qptr<Ui::FadeShadow> _topShadow, _bottomShadow;
 
 	base::unique_qptr<Ui::AbstractSinglePreview> _content;
 	base::unique_qptr<ChatHelpers::TabbedPanel> _emojiPanel;
@@ -93,15 +93,16 @@ private:
 
 	mtpRequestId _saveRequestId = 0;
 
+	bool _isPhoto = false;
 	bool _asFile = false;
 
 	QString _error;
 
-	rpl::variable<bool> _isPhoto = false;
 	rpl::variable<int> _footerHeight = 0;
 
 	rpl::event_stream<> _editMediaClicks;
 	rpl::event_stream<> _photoEditorOpens;
+	rpl::event_stream<> _previewRebuilds;
 	rpl::event_stream<int> _contentHeight;
 
 };