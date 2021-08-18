dmytrokurochkin/MZGram-Desktop

commit ec4e520d860e78da16bd54c19c1bea2abcb3b7b9

Fix -Wunused-variable warnings

Telegram/SourceFiles/window/themes/window_theme_editor.cpp
@@ -164,16 +164,13 @@ bool isValidColorValue(QLatin1String value) {
 		skipWhitespacesAndComments(data, end);
 		if (data == end) break;
 
-		auto foundName = base::parse::readName(data, end);
 		skipWhitespacesAndComments(data, end);
 		if (data == end || *data != ':') {
 			return "error";
 		}
 		++data;
 		skipWhitespacesAndComments(data, end);
-		auto valueStart = data;
 		auto value = readValue(data, end);
-		auto valueEnd = data;
 		if (value.size() == 0) {
 			return "error";
 		}

commit 4e2aed0041102d8609c7349422b6ff770e2a8cc5

Fix creating a theme from a default one.
Regression was introduced in ec4e520d86.

Telegram/SourceFiles/window/themes/window_theme_editor.cpp
@@ -164,6 +164,7 @@ bool isValidColorValue(QLatin1String value) {
 		skipWhitespacesAndComments(data, end);
 		if (data == end) break;
 
+		[[maybe_unused]] auto foundName = base::parse::readName(data, end);
 		skipWhitespacesAndComments(data, end);
 		if (data == end || *data != ':') {
 			return "error";