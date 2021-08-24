dmytrokurochkin/MZGram-Desktop

commit 81927d7c5e4e121c341db103b46ce8f53402b480

Allow arbitrary QImage as outgoing bubbles background.

Telegram/SourceFiles/history/view/history_view_list_widget.cpp
@@ -1612,17 +1612,22 @@ void ListWidget::paintEvent(QPaintEvent *e) {
 		return this->itemTop(elem) < bottom;
 	});
 	if (from != end(_items)) {
+		auto viewport = QRect(); // #TODO bubbles
 		auto top = itemTop(from->get());
+		auto context = HistoryView::PaintContext{
+			.bubblesPattern = nullptr,
+			.viewport = viewport.translated(0, -top),
+			.clip = clip.translated(0, -top),
+			.now = crl::now(),
+		};
 		p.translate(0, top);
 		for (auto i = from; i != to; ++i) {
 			const auto view = *i;
-			view->draw(
-				p,
-				clip.translated(0, -top),
-				itemRenderSelection(view),
-				ms);
+			view->draw(p, context);
 			const auto height = view->height();
 			top += height;
+			context.viewport.translate(0, -height);
+			context.clip.translate(0, -height);
 			p.translate(0, height);
 		}
 		p.translate(0, -top);

Telegram/SourceFiles/boxes/background_preview_box.cpp
@@ -589,11 +589,18 @@ QRect BackgroundPreviewBox::radialRect() const {
 void BackgroundPreviewBox::paintTexts(Painter &p, crl::time ms) {
 	const auto height1 = _text1->height();
 	const auto height2 = _text2->height();
+	const auto context = HistoryView::PaintContext{
+		.bubblesPattern = nullptr, // #TODO bubbles
+		.viewport = rect(),
+		.clip = rect(),
+		.selection = TextSelection(),
+		.now = ms,
+	};
 	p.translate(0, textsTop());
 	paintDate(p);
-	_text1->draw(p, rect(), TextSelection(), ms);
+	_text1->draw(p, context);
 	p.translate(0, height1);
-	_text2->draw(p, rect(), TextSelection(), ms);
+	_text2->draw(p, context);
 	p.translate(0, height2);
 }
 

Telegram/SourceFiles/history/admin_log/history_admin_log_inner.cpp
@@ -895,9 +895,7 @@ void InnerWidget::paintEvent(QPaintEvent *e) {
 
 	Painter p(this);
 
-	auto ms = crl::now();
 	auto clip = e->rect();
-
 	if (_items.empty() && _upLoaded && _downLoaded) {
 		paintEmpty(p);
 	} else {
@@ -914,16 +912,26 @@ void InnerWidget::paintEvent(QPaintEvent *e) {
 			return this->itemTop(elem) < bottom;
 		});
 		if (from != end) {
+			auto viewport = QRect(); // #TODO bubbles
 			auto top = itemTop(from->get());
+			auto context = HistoryView::PaintContext{
+				.bubblesPattern = nullptr,
+				.viewport = viewport.translated(0, -top),
+				.clip = clip.translated(0, -top),
+				.now = crl::now(),
+			};
 			p.translate(0, top);
 			for (auto i = from; i != to; ++i) {
 				const auto view = i->get();
-				const auto selection = (view == _selectedItem)
+				context.selection = (view == _selectedItem)
 					? _selectedText
 					: TextSelection();
-				view->draw(p, clip.translated(0, -top), selection, ms);
-				auto height = view->height();
+				view->draw(p, context);
+
+				const auto height = view->height();
 				top += height;
+				context.viewport.translate(0, -height);
+				context.clip.translate(0, -height);
 				p.translate(0, height);
 			}
 			p.translate(0, -top);

Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -560,7 +560,6 @@ void HistoryInner::paintEvent(QPaintEvent *e) {
 
 	Painter p(this);
 	auto clip = e->rect();
-	auto ms = crl::now();
 
 	const auto historyDisplayedEmpty = _history->isDisplayedEmpty()
 		&& (!_migrated || _migrated->isDisplayedEmpty());
@@ -602,6 +601,8 @@ void HistoryInner::paintEvent(QPaintEvent *e) {
 		} else {
 			seltoy += _dragSelTo->height();
 		}
+		const auto visibleAreaTopGlobal = mapToGlobal(
+			QPoint(0, _visibleAreaTop)).y();
 
 		auto mtop = migratedTop();
 		auto htop = historyTop();
@@ -613,15 +614,20 @@ void HistoryInner::paintEvent(QPaintEvent *e) {
 			auto view = block->messages[iItem].get();
 			auto item = view->data();
 
-			auto y = mtop + block->y() + view->y();
-			p.save();
-			p.translate(0, y);
-			if (clip.y() < y + view->height()) while (y < drawToY) {
-				const auto selection = itemRenderSelection(
+			auto top = mtop + block->y() + view->y();
+			auto context = _controller->bubblesContext({
+				.visibleAreaTop = _visibleAreaTop,
+				.visibleAreaTopGlobal = visibleAreaTopGlobal,
+				.clip = clip,
+				.initialShift = top,
+			});
+			p.translate(0, top);
+			if (context.clip.y() < view->height()) while (top < drawToY) {
+				context.selection = itemRenderSelection(
 					view,
 					selfromy - mtop,
 					seltoy - mtop);
-				view->draw(p, clip.translated(0, -y), selection, ms);
+				view->draw(p, context);
 
 				if (item->hasViews()) {
 					_controller->content()->scheduleViewIncrement(item);
@@ -631,9 +637,11 @@ void HistoryInner::paintEvent(QPaintEvent *e) {
 					_widget->enqueueMessageHighlight(view);
 				}
 
-				int32 h = view->height();
-				p.translate(0, h);
-				y += h;
+				const auto height = view->height();
+				top += height;
+				context.viewport.translate(0, -height);
+				context.clip.translate(0, -height);
+				p.translate(0, height);
 
 				++iItem;
 				if (iItem == block->messages.size()) {
@@ -647,7 +655,7 @@ void HistoryInner::paintEvent(QPaintEvent *e) {
 				view = block->messages[iItem].get();
 				item = view->data();
 			}
-			p.restore();
+			p.translate(0, -top);
 		}
 		if (htop >= 0) {
 			auto iBlock = (_curHistory == _history ? _curBlock : 0);
@@ -656,21 +664,28 @@ void HistoryInner::paintEvent(QPaintEvent *e) {
 			auto view = block->messages[iItem].get();
 			auto item = view->data();
 			auto readTill = (HistoryItem*)nullptr;
-			auto hclip = clip.intersected(QRect(0, hdrawtop, width(), clip.top() + clip.height()));
-			auto y = htop + block->y() + view->y();
-			p.save();
-			p.translate(0, y);
-			while (y < drawToY) {
-				const auto h = view->height();
-				if (hclip.y() < y + h && hdrawtop < y + h) {
-					const auto selection = itemRenderSelection(
+			auto top = htop + block->y() + view->y();
+			auto context = _controller->bubblesContext({
+				.visibleAreaTop = _visibleAreaTop,
+				.visibleAreaTopGlobal = visibleAreaTopGlobal,
+				.visibleAreaWidth = width(),
+				.clip = clip.intersected(
+					QRect(0, hdrawtop, width(), clip.top() + clip.height())
+				),
+				.initialShift = top,
+			});
+			p.translate(0, top);
+			while (top < drawToY) {
+				const auto height = view->height();
+				if (context.clip.y() < height && hdrawtop < top + height) {
+					context.selection = itemRenderSelection(
 						view,
 						selfromy - htop,
 						seltoy - htop);
-					view->draw(p, hclip.translated(0, -y), selection, ms);
+					view->draw(p, context);
 
-					const auto middle = y + h / 2;
-					const auto bottom = y + h;
+					const auto middle = top + height / 2;
+					const auto bottom = top + height;
 					if (_visibleAreaBottom >= bottom) {
 						const auto item = view->data();
 						if (!item->out() && item->unread()) {
@@ -688,8 +703,10 @@ void HistoryInner::paintEvent(QPaintEvent *e) {
 						}
 					}
 				}
-				p.translate(0, h);
-				y += h;
+				top += height;
+				context.viewport.translate(0, -height);
+				context.clip.translate(0, -height);
+				p.translate(0, height);
 
 				++iItem;
 				if (iItem == block->messages.size()) {
@@ -703,7 +720,7 @@ void HistoryInner::paintEvent(QPaintEvent *e) {
 				view = block->messages[iItem].get();
 				item = view->data();
 			}
-			p.restore();
+			p.translate(0, -top);
 
 			if (readTill && _widget->doWeReadServerHistory()) {
 				session().data().histories().readInboxTill(readTill);

Telegram/SourceFiles/history/view/history_view_element.h
@@ -24,6 +24,7 @@ class SessionController;
 
 namespace Ui {
 class PathShiftGradient;
+struct BubblePattern;
 } // namespace Ui
 
 namespace HistoryView {
@@ -191,6 +192,14 @@ struct DateBadge : public RuntimeComponent<DateBadge, Element> {
 
 };
 
+struct PaintContext {
+	const Ui::BubblePattern *bubblesPattern = nullptr;
+	QRect viewport;
+	QRect clip;
+	TextSelection selection;
+	crl::time now = 0;
+};
+
 class Element
 	: public Object
 	, public RuntimeComposer<Element>
@@ -261,11 +270,7 @@ public:
 	bool displayDate() const;
 	bool isInOneDayWithPrevious() const;
 
-	virtual void draw(
-		Painter &p,
-		QRect clip,
-		TextSelection selection,
-		crl::time ms) const = 0;
+	virtual void draw(Painter &p, const PaintContext &context) const = 0;
 	[[nodiscard]] virtual PointState pointState(QPoint point) const = 0;
 	[[nodiscard]] virtual TextState textState(
 		QPoint point,

Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -17,6 +17,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "history/history.h"
 #include "ui/effects/ripple_animation.h"
 #include "base/unixtime.h"
+#include "ui/chat/message_bubble.h"
 #include "ui/toast/toast.h"
 #include "ui/text/text_utilities.h"
 #include "ui/text/text_entity.h"
@@ -147,103 +148,6 @@ int KeyboardStyle::minButtonWidth(
 
 QString FastReplyText() {
 	return tr::lng_fast_reply(tr::now);
-}
-
-void PaintBubble(Painter &p, QRect rect, int outerWidth, bool selected, bool outbg, RectPart tailSide, RectParts skip) {
-	auto &bg = selected ? (outbg ? st::msgOutBgSelected : st::msgInBgSelected) : (outbg ? st::msgOutBg : st::msgInBg);
-	auto sh = &(selected ? (outbg ? st::msgOutShadowSelected : st::msgInShadowSelected) : (outbg ? st::msgOutShadow : st::msgInShadow));
-	auto cors = selected ? (outbg ? Ui::MessageOutSelectedCorners : Ui::MessageInSelectedCorners) : (outbg ? Ui::MessageOutCorners : Ui::MessageInCorners);
-	auto parts = RectPart::None | RectPart::NoTopBottom;
-	if (skip & RectPart::Top) {
-		if (skip & RectPart::Bottom) {
-			p.fillRect(rect, bg);
-			return;
-		}
-		rect.setTop(rect.y() - st::historyMessageRadius);
-	} else {
-		parts |= RectPart::FullTop;
-	}
-	if (skip & RectPart::Bottom) {
-		rect.setHeight(rect.height() + st::historyMessageRadius);
-		sh = nullptr;
-		tailSide = RectPart::None;
-	} else {
-		parts |= RectPart::Bottom;
-	}
-	if (tailSide == RectPart::Right) {
-		parts |= RectPart::BottomLeft;
-		p.fillRect(rect.x() + rect.width() - st::historyMessageRadius, rect.y() + rect.height() - st::historyMessageRadius, st::historyMessageRadius, st::historyMessageRadius, bg);
-		auto &tail = selected ? st::historyBubbleTailOutRightSelected : st::historyBubbleTailOutRight;
-		tail.paint(p, rect.x() + rect.width(), rect.y() + rect.height() - tail.height(), outerWidth);
-		p.fillRect(rect.x() + rect.width() - st::historyMessageRadius, rect.y() + rect.height(), st::historyMessageRadius + tail.width(), st::msgShadow, *sh);
-	} else if (tailSide == RectPart::Left) {
-		parts |= RectPart::BottomRight;
-		p.fillRect(rect.x(), rect.y() + rect.height() - st::historyMessageRadius, st::historyMessageRadius, st::historyMessageRadius, bg);
-		auto &tail = selected ? (outbg ? st::historyBubbleTailOutLeftSelected : st::historyBubbleTailInLeftSelected) : (outbg ? st::historyBubbleTailOutLeft : st::historyBubbleTailInLeft);
-		tail.paint(p, rect.x() - tail.width(), rect.y() + rect.height() - tail.height(), outerWidth);
-		p.fillRect(rect.x() - tail.width(), rect.y() + rect.height(), st::historyMessageRadius + tail.width(), st::msgShadow, *sh);
-	} else if (!(skip & RectPart::Bottom)) {
-		parts |= RectPart::FullBottom;
-	}
-	Ui::FillRoundRect(p, rect, bg, cors, sh, parts);
-}
-
-void PaintBubble(Painter &p, QRect rect, int outerWidth, bool selected, const std::vector<BubbleSelectionInterval> &selection, bool outbg, RectPart tailSide) {
-	if (selection.empty()) {
-		PaintBubble(
-			p,
-			rect,
-			outerWidth,
-			selected,
-			outbg,
-			tailSide,
-			RectPart::None);
-		return;
-	}
-	const auto left = rect.x();
-	const auto width = rect.width();
-	const auto top = rect.y();
-	const auto bottom = top + rect.height();
-	auto from = top;
-	for (const auto &selected : selection) {
-		if (selected.top > from) {
-			const auto skip = RectPart::Bottom
-				| (from > top ? RectPart::Top : RectPart::None);
-			PaintBubble(
-				p,
-				QRect(left, from, width, selected.top - from),
-				outerWidth,
-				false,
-				outbg,
-				tailSide,
-				skip);
-		}
-		const auto skip = ((selected.top > top)
-			? RectPart::Top
-			: RectPart::None)
-			| ((selected.top + selected.height < bottom)
-				? RectPart::Bottom
-				: RectPart::None);
-		PaintBubble(
-			p,
-			QRect(left, selected.top, width, selected.height),
-			outerWidth,
-			true,
-			outbg,
-			tailSide,
-			skip);
-		from = selected.top + selected.height;
-	}
-	if (from < bottom) {
-		PaintBubble(
-			p,
-			QRect(left, from, width, bottom - from),
-			outerWidth,
-			false,
-			outbg,
-			tailSide,
-			RectPart::Top);
-	}
 }
 
 style::color FromNameFg(PeerId peerId, bool selected) {
@@ -545,11 +449,7 @@ int Message::marginBottom() const {
 	return isHidden() ? 0 : st::msgMargin.bottom();
 }
 
-void Message::draw(
-		Painter &p,
-		QRect clip,
-		TextSelection selection,
-		crl::time ms) const {
+void Message::draw(Painter &p, const PaintContext &context) const {
 	auto g = countGeometry();
 	if (g.width() < 1) {
 		return;
@@ -560,7 +460,7 @@ void Message::draw(
 
 	const auto outbg = hasOutLayout();
 	const auto bubble = drawBubble();
-	const auto selected = (selection == FullSelection);
+	const auto selected = (context.selection == FullSelection);
 
 	auto dateh = 0;
 	if (const auto date = Get<DateBadge>()) {
@@ -568,7 +468,7 @@ void Message::draw(
 	}
 	if (const auto bar = Get<UnreadBar>()) {
 		auto unreadbarh = bar->height();
-		if (clip.intersects(QRect(0, dateh, width(), unreadbarh))) {
+		if (context.clip.intersects(QRect(0, dateh, width(), unreadbarh))) {
 			p.translate(0, dateh);
 			bar->paint(p, 0, width(), delegate()->elementIsChatWide());
 			p.translate(0, -dateh);
@@ -587,8 +487,8 @@ void Message::draw(
 	auto mediaOnTop = (mediaDisplayed && media->isBubbleTop()) || (entry && entry->isBubbleTop());
 
 	auto mediaSelectionIntervals = (!selected && mediaDisplayed)
-		? media->getBubbleSelectionIntervals(selection)
-		: std::vector<BubbleSelectionInterval>();
+		? media->getBubbleSelectionIntervals(context.selection)
+		: std::vector<Ui::BubbleSelectionInterval>();
 	auto localMediaTop = 0;
 	const auto customHighlight = mediaDisplayed && media->customHighlight();
 	if (!mediaSelectionIntervals.empty() || customHighlight) {
@@ -633,7 +533,7 @@ void Message::draw(
 		g.setHeight(g.height() - keyboardHeight);
 		auto keyboardPosition = QPoint(g.left(), g.top() + g.height() + st::msgBotKbButton.margin);
 		p.translate(keyboardPosition);
-		keyboard->paint(p, g.width(), clip.translated(-keyboardPosition));
+		keyboard->paint(p, g.width(), context.clip.translated(-keyboardPosition));
 		p.translate(-keyboardPosition);
 	}
 
@@ -644,23 +544,30 @@ void Message::draw(
 			fromNameUpdated(g.width());
 		}
 
-		auto skipTail = isAttachedToNext()
+		const auto skipTail = isAttachedToNext()
 			|| (media && media->skipBubbleTail())
 			|| (keyboard != nullptr)
-			|| (context() == Context::Replies && data()->isDiscussionPost());
-		auto displayTail = skipTail
+			|| (this->context() == Context::Replies
+				&& data()->isDiscussionPost());
+		const auto displayTail = skipTail
 			? RectPart::None
 			: (outbg && !delegate()->elementIsChatWide())
 			? RectPart::Right
 			: RectPart::Left;
-		PaintBubble(
+		Ui::PaintBubble(
 			p,
-			g,
-			width(),
-			selected,
-			mediaSelectionIntervals,
-			outbg,
-			displayTail);
+			Ui::ComplexBubble{
+				.simple = Ui::SimpleBubble{
+					.geometry = g,
+					.pattern = context.bubblesPattern,
+					.patternViewport = context.viewport,
+					.outerWidth = width(),
+					.selected = selected,
+					.outbg = outbg,
+					.tailSide = displayTail,
+				},
+				.selection = mediaSelectionIntervals,
+			});
 
 		auto inner = g;
 		paintCommentsButton(p, inner, selected);
@@ -680,25 +587,32 @@ void Message::draw(
 		if (entry) {
 			trect.setHeight(trect.height() - entry->height());
 		}
-		paintText(p, trect, selection);
+		paintText(p, trect, context.selection);
 		if (mediaDisplayed) {
 			auto mediaHeight = media->height();
 			auto mediaLeft = inner.left();
 			auto mediaTop = (trect.y() + trect.height() - mediaHeight);
 
 			p.translate(mediaLeft, mediaTop);
-			media->draw(p, clip.translated(-mediaLeft, -mediaTop), skipTextSelection(selection), ms);
+			media->draw(
+				p,
+				context.clip.translated(-mediaLeft, -mediaTop),
+				skipTextSelection(context.selection), context.now);
 			p.translate(-mediaLeft, -mediaTop);
 		}
 		if (entry) {
 			auto entryLeft = inner.left();
 			auto entryTop = trect.y() + trect.height();
 			p.translate(entryLeft, entryTop);
-			auto entrySelection = skipTextSelection(selection);
+			auto entrySelection = skipTextSelection(context.selection);
 			if (mediaDisplayed) {
 				entrySelection = media->skipSelection(entrySelection);
 			}
-			entry->draw(p, clip.translated(-entryLeft, -entryTop), entrySelection, ms);
+			entry->draw(
+				p,
+				context.clip.translated(-entryLeft, -entryTop),
+				entrySelection,
+				context.now);
 			p.translate(-entryLeft, -entryTop);
 		}
 		const auto needDrawInfo = entry
@@ -734,11 +648,15 @@ void Message::draw(
 		}
 
 		if (media) {
-			media->paintBubbleFireworks(p, g, ms);
+			media->paintBubbleFireworks(p, g, context.now);
 		}
 	} else if (media && media->isDisplayed()) {
 		p.translate(g.topLeft());
-		media->draw(p, clip.translated(-g.topLeft()), skipTextSelection(selection), ms);
+		media->draw(
+			p,
+			context.clip.translated(-g.topLeft()),
+			skipTextSelection(context.selection),
+			context.now);
 		p.translate(-g.topLeft());
 	}
 

Telegram/SourceFiles/history/view/history_view_message.h
@@ -51,11 +51,7 @@ public:
 
 	int marginTop() const override;
 	int marginBottom() const override;
-	void draw(
-		Painter &p,
-		QRect clip,
-		TextSelection selection,
-		crl::time ms) const override;
+	void draw(Painter &p, const PaintContext &context) const override;
 	PointState pointState(QPoint point) const override;
 	TextState textState(
 		QPoint point,

Telegram/SourceFiles/history/view/history_view_service_message.cpp
@@ -519,11 +519,7 @@ int Service::marginBottom() const {
 	return st::msgServiceMargin.bottom();
 }
 
-void Service::draw(
-		Painter &p,
-		QRect clip,
-		TextSelection selection,
-		crl::time ms) const {
+void Service::draw(Painter &p, const PaintContext &context) const {
 	const auto item = message();
 	auto g = countGeometry();
 	if (g.width() < 1) {
@@ -533,6 +529,7 @@ void Service::draw(
 	auto height = this->height() - st::msgServiceMargin.top() - st::msgServiceMargin.bottom();
 	auto dateh = 0;
 	auto unreadbarh = 0;
+	auto clip = context.clip;
 	if (auto date = Get<DateBadge>()) {
 		dateh = date->height();
 		p.translate(0, dateh);
@@ -564,7 +561,7 @@ void Service::draw(
 		height -= st::msgServiceMargin.top() + media->height();
 		auto left = st::msgServiceMargin.left() + (g.width() - media->maxWidth()) / 2, top = st::msgServiceMargin.top() + height + st::msgServiceMargin.top();
 		p.translate(left, top);
-		media->draw(p, clip.translated(-left, -top), TextSelection(), ms);
+		media->draw(p, clip.translated(-left, -top), TextSelection(), context.now);
 		p.translate(-left, -top);
 	}
 
@@ -575,7 +572,7 @@ void Service::draw(
 	p.setBrush(Qt::NoBrush);
 	p.setPen(st::msgServiceFg);
 	p.setFont(st::msgServiceFont);
-	item->_text.draw(p, trect.x(), trect.y(), trect.width(), Qt::AlignCenter, 0, -1, selection, false);
+	item->_text.draw(p, trect.x(), trect.y(), trect.width(), Qt::AlignCenter, 0, -1, context.selection, false);
 
 	p.restoreTextPalette();
 

commit d3703f52bf47b61add273340df9e053cce992b27

Fixed render of text selection in sections.
Regression was introduced in 81927d7c5e.

Telegram/SourceFiles/history/view/history_view_list_widget.cpp
@@ -1623,6 +1623,7 @@ void ListWidget::paintEvent(QPaintEvent *e) {
 		p.translate(0, top);
 		for (auto i = from; i != to; ++i) {
 			const auto view = *i;
+			context.selection = itemRenderSelection(view);
 			view->draw(p, context);
 			const auto height = view->height();
 			top += height;