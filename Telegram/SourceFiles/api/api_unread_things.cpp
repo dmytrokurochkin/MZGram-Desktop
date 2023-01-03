dmytrokurochkin/MZGram-Desktop

commit dac360c40bfa6fe5f9e0faaa044e96d2e7bddd52

Update API scheme on layer 148.
Extract message history corner buttons code.

Telegram/SourceFiles/api/api_unread_things.cpp
@@ -41,24 +41,22 @@ constexpr auto kNextRequestLimit = 100;
 UnreadThings::UnreadThings(not_null<ApiWrap*> api) : _api(api) {
 }
 
-bool UnreadThings::trackMentions(PeerData *peer) const {
+bool UnreadThings::trackMentions(DialogsEntry *entry) const {
+	const auto peer = entry ? ResolveHistory(entry)->peer.get() : nullptr;
 	return peer && (peer->isChat() || peer->isMegagroup());
 }
 
-bool UnreadThings::trackReactions(PeerData *peer) const {
-	return trackMentions(peer) || (peer && peer->isUser());
+bool UnreadThings::trackReactions(DialogsEntry *entry) const {
+	const auto peer = entry ? ResolveHistory(entry)->peer.get() : nullptr;
+	return peer && (peer->isChat() || peer->isMegagroup());
 }
 
 void UnreadThings::preloadEnough(DialogsEntry *entry) {
-	if (!entry) {
-		return;
+	if (trackMentions(entry)) {
+		preloadEnoughMentions(entry);
 	}
-	const auto history = ResolveHistory(entry);
-	if (trackMentions(history->peer)) {
-		preloadEnoughMentions(history);
-	}
-	if (trackReactions(history->peer)) {
-		preloadEnoughReactions(history);
+	if (trackReactions(entry)) {
+		preloadEnoughReactions(entry);
 	}
 }
 

Telegram/CMakeLists.txt
@@ -690,6 +690,8 @@ PRIVATE
     history/view/history_view_contact_status.h
     history/view/history_view_context_menu.cpp
     history/view/history_view_context_menu.h
+    history/view/history_view_corner_buttons.cpp
+    history/view/history_view_corner_buttons.h
     history/view/history_view_cursor_state.cpp
     history/view/history_view_cursor_state.h
     history/view/history_view_element.cpp

Telegram/Resources/tl/api.tl
@@ -222,6 +222,7 @@ inputNotifyPeer#b8bc5b0c peer:InputPeer = InputNotifyPeer;
 inputNotifyUsers#193b4417 = InputNotifyPeer;
 inputNotifyChats#4a95e84e = InputNotifyPeer;
 inputNotifyBroadcasts#b1db7c7e = InputNotifyPeer;
+inputNotifyForumTopic#5c467992 peer:InputPeer top_msg_id:int = InputNotifyPeer;
 
 inputPeerNotifySettings#df1f002b flags:# show_previews:flags.0?Bool silent:flags.1?Bool mute_until:flags.2?int sound:flags.3?NotificationSound = InputPeerNotifySettings;
 
@@ -1454,7 +1455,7 @@ stickerKeyword#fcfeb29c document_id:long keyword:Vector<string> = StickerKeyword
 
 username#b4073647 flags:# editable:flags.0?true active:flags.1?true username:string = Username;
 
-forumTopic#18a9a864 flags:# my:flags.1?true id:int date:int title:string icon_color:int icon_emoji_id:flags.0?long top_message:int read_inbox_max_id:int read_outbox_max_id:int unread_count:int unread_mentions_count:int unread_reactions_count:int from_id:Peer = ForumTopic;
+forumTopic#5920d6dc flags:# my:flags.1?true id:int date:int title:string icon_color:int icon_emoji_id:flags.0?long top_message:int read_inbox_max_id:int read_outbox_max_id:int unread_count:int unread_mentions_count:int unread_reactions_count:int from_id:Peer notify_settings:PeerNotifySettings = ForumTopic;
 
 messages.forumTopics#367617d3 flags:# order_by_create_date:flags.0?true count:int topics:Vector<ForumTopic> messages:Vector<Message> chats:Vector<Chat> users:Vector<User> pts:int = messages.ForumTopics;
 
@@ -1704,7 +1705,7 @@ messages.getEmojiKeywords#35a0e062 lang_code:string = EmojiKeywordsDifference;
 messages.getEmojiKeywordsDifference#1508b6af lang_code:string from_version:int = EmojiKeywordsDifference;
 messages.getEmojiKeywordsLanguages#4e9963b2 lang_codes:Vector<string> = Vector<EmojiLanguage>;
 messages.getEmojiURL#d5b10c26 lang_code:string = EmojiURL;
-messages.getSearchCounters#732eef00 peer:InputPeer filters:Vector<MessagesFilter> = Vector<messages.SearchCounter>;
+messages.getSearchCounters#ae7cc1 flags:# peer:InputPeer top_msg_id:flags.0?int filters:Vector<MessagesFilter> = Vector<messages.SearchCounter>;
 messages.requestUrlAuth#198fb446 flags:# peer:flags.1?InputPeer msg_id:flags.1?int button_id:flags.1?int url:flags.2?string = UrlAuthResult;
 messages.acceptUrlAuth#b12c7125 flags:# write_allowed:flags.0?true peer:flags.1?InputPeer msg_id:flags.1?int button_id:flags.1?int url:flags.2?string = UrlAuthResult;
 messages.hidePeerSettingsBar#4facb138 peer:InputPeer = Bool;
@@ -1754,7 +1755,7 @@ messages.getAvailableReactions#18dea0ac hash:int = messages.AvailableReactions;
 messages.setDefaultReaction#4f47a016 reaction:Reaction = Bool;
 messages.translateText#24ce6dee flags:# peer:flags.0?InputPeer msg_id:flags.0?int text:flags.1?string from_lang:flags.2?string to_lang:string = messages.TranslatedText;
 messages.getUnreadReactions#3223495b flags:# peer:InputPeer top_msg_id:flags.0?int offset_id:int add_offset:int limit:int max_id:int min_id:int = messages.Messages;
-messages.readReactions#82e251d7 peer:InputPeer = messages.AffectedHistory;
+messages.readReactions#54aa7f8e flags:# peer:InputPeer top_msg_id:flags.0?int = messages.AffectedHistory;
 messages.searchSentMedia#107e31a0 q:string filter:MessagesFilter limit:int = messages.Messages;
 messages.getAttachMenuBots#16fcc2cb hash:long = AttachMenuBots;
 messages.getAttachMenuBot#77216192 bot:InputUser = AttachMenuBotsBot;
@@ -1865,6 +1866,7 @@ channels.createForumTopic#f40c0224 flags:# channel:InputChannel title:string ico
 channels.getForumTopics#de560d1 flags:# channel:InputChannel q:flags.0?string offset_date:int offset_id:int offset_topic:int limit:int = messages.ForumTopics;
 channels.getForumTopicsByID#b0831eb9 channel:InputChannel topics:Vector<int> = messages.ForumTopics;
 channels.editForumTopic#8a7f464b flags:# channel:InputChannel topic_id:int title:flags.0?string icon_emoji_id:flags.1?long = Updates;
+channels.deleteTopicHistory#34435f2d channel:InputChannel top_msg_id:int = messages.AffectedHistory;
 
 bots.sendCustomRequest#aa2769ed custom_method:string params:DataJSON = DataJSON;
 bots.answerWebhookJSONQuery#e6213f4d query_id:long data:DataJSON = Bool;

Telegram/SourceFiles/api/api_unread_things.h
@@ -23,8 +23,8 @@ public:
 
 	explicit UnreadThings(not_null<ApiWrap*> api);
 
-	[[nodiscard]] bool trackMentions(PeerData *peer) const;
-	[[nodiscard]] bool trackReactions(PeerData *peer) const;
+	[[nodiscard]] bool trackMentions(DialogsEntry *entry) const;
+	[[nodiscard]] bool trackReactions(DialogsEntry *entry) const;
 
 	void preloadEnough(DialogsEntry *entry);
 

Telegram/SourceFiles/data/data_folder.h
@@ -22,7 +22,7 @@ namespace Data {
 
 class Session;
 
-class Folder final : public Dialogs::Entry, public base::has_weak_ptr {
+class Folder final : public Dialogs::Entry {
 public:
 	static constexpr auto kId = 1;
 

Telegram/SourceFiles/data/data_forum.cpp
@@ -239,6 +239,12 @@ void Forum::clearAllUnreadMentions() {
 	}
 }
 
+void Forum::clearAllUnreadReactions() {
+	for (const auto &[rootId, topic] : _topics) {
+		topic->unreadReactions().clear();
+	}
+}
+
 ForumTopic *Forum::topicFor(not_null<const HistoryItem*> item) {
 	const auto maybe = topicFor(item->replyToTop());
 	return maybe ? maybe : topicFor(item->topicRootId());

Telegram/SourceFiles/data/data_forum.h
@@ -60,6 +60,7 @@ public:
 	void created(MsgId rootId, MsgId realId);
 
 	void clearAllUnreadMentions();
+	void clearAllUnreadReactions();
 
 private:
 	struct TopicRequest {

Telegram/SourceFiles/data/data_search_controller.h
@@ -10,8 +10,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/data_sparse_ids.h"
 #include "storage/storage_sparse_ids_list.h"
 #include "storage/storage_shared_media.h"
-#include "base/value_ordering.h"
 #include "base/timer.h"
+#include "base/qt/qt_compare.h"
 
 namespace Main {
 class Session;
@@ -58,13 +58,9 @@ public:
 		QString query;
 		// from_id, min_date, max_date
 
-		friend inline auto value_ordering_helper(const Query &value) {
-			return std::tie(
-				value.peerId,
-				value.migratedPeerId,
-				value.type,
-				value.query);
-		}
+		friend inline std::strong_ordering operator<=>(
+			const Query &a,
+			const Query &b) noexcept = default;
 
 	};
 	struct SavedState {

commit 8edcd6d390120e215dc51e9331df95627a02b2df

Fix unread reactions button in chats with users.
Regression was introduced in dac360c40b.

Fixes #25661.

Telegram/SourceFiles/api/api_unread_things.cpp
@@ -36,7 +36,7 @@ bool UnreadThings::trackMentions(Data::Thread *thread) const {
 
 bool UnreadThings::trackReactions(Data::Thread *thread) const {
 	const auto peer = thread ? thread->peer().get() : nullptr;
-	return peer && (peer->isChat() || peer->isMegagroup());
+	return peer && (peer->isUser() || peer->isChat() || peer->isMegagroup());
 }
 
 void UnreadThings::preloadEnough(Data::Thread *thread) {