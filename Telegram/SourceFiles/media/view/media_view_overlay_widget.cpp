dmytrokurochkin/MZGram-Desktop

commit 10734c09008bac091db68c99d51b7f52a44d65d3

Improve silent stories playback.

Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -3698,6 +3698,7 @@ bool OverlayWidget::createStreamingObjects() {
 	_streamed->instance.setPriority(kOverlayLoaderPriority);
 	_streamed->instance.lockPlayer();
 	_streamed->withSound = _document
+		&& !_document->isSilentVideo()
 		&& (_document->isAudioFile()
 			|| _document->isVideoFile()
 			|| _document->isVoiceMessage()
@@ -4038,12 +4039,14 @@ void OverlayWidget::restartAtSeekPosition(crl::time position) {
 	};
 	if (!_streamed->withSound) {
 		options.mode = Streaming::Mode::Video;
-		options.loop = true;
+		options.loop = !_stories;
 	} else {
 		Assert(_document != nullptr);
 		const auto messageId = _message ? _message->fullId() : FullMsgId();
 		options.audioId = AudioMsgId(_document, messageId);
-		options.speed = Core::App().settings().videoPlaybackSpeed();
+		options.speed = _stories
+			? Core::App().settings().videoPlaybackSpeed()
+			: 1.;
 		if (_pip) {
 			_pip = nullptr;
 		}
@@ -4111,7 +4114,7 @@ void OverlayWidget::playbackControlsSpeedChanged(float64 speed) {
 		Core::App().settings().setVideoPlaybackSpeed(speed);
 		Core::App().saveSettingsDelayed();
 	}
-	if (_streamed && _streamed->controls) {
+	if (_streamed && _streamed->controls && !_stories) {
 		DEBUG_LOG(("Media playback speed: %1 to _streamed.").arg(speed));
 		_streamed->instance.setSpeed(speed);
 	}

commit d8a3b6b286f766f649f7cd75fe722dbc4c333280

Fix initial video playback speed.
Regression was introduced in 10734c0900.

Telegram/SourceFiles/media/view/media_view_overlay_widget.cpp
@@ -4051,8 +4051,8 @@ void OverlayWidget::restartAtSeekPosition(crl::time position) {
 		const auto messageId = _message ? _message->fullId() : FullMsgId();
 		options.audioId = AudioMsgId(_document, messageId);
 		options.speed = _stories
-			? Core::App().settings().videoPlaybackSpeed()
-			: 1.;
+			? 1.
+			: Core::App().settings().videoPlaybackSpeed();
 		if (_pip) {
 			_pip = nullptr;
 		}