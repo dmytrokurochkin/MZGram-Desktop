dmytrokurochkin/MZGram-Desktop

commit 30fe873daef36e0f56b49c9fb3287fc5c8e32601

Fixed initial tray icon color on macOS dark theme.

Telegram/SourceFiles/platform/mac/tray_mac.mm
@@ -174,7 +174,7 @@ void UpdateIcon(const NSStatusItem *status) {
 		return;
 	}
 
-	const auto appearance = status.button.effectiveAppearance;
+	const auto appearance = [NSApp effectiveAppearance];
 	const auto darkMode = [[appearance.name lowercaseString]
 		containsString:@"dark"];
 

commit f21cdbc9b20d99e6e496fcc19553d36d8082fdd1

Fixed crash on macOS 10.13.
Regression was introduced in 30fe873dae.

Telegram/SourceFiles/platform/mac/tray_mac.mm
@@ -174,7 +174,12 @@ void UpdateIcon(const NSStatusItem *status) {
 		return;
 	}
 
-	const auto appearance = [NSApp effectiveAppearance];
+	const auto appearance = [&] {
+		if (@available(macOS 10.14, *)) {
+			return [NSApp effectiveAppearance];
+		}
+		return status.button.effectiveAppearance;
+	}();
 	const auto darkMode = [[appearance.name lowercaseString]
 		containsString:@"dark"];
 