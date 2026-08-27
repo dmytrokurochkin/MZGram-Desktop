dmytrokurochkin/MZGram-Desktop

commit f8fe46b5a3d73762c607026951345213ee8bbcc2

[vid-editor] Added GIF toggle.

Telegram/SourceFiles/api/api_media.cpp
@@ -31,6 +31,9 @@ MTPVector<MTPDocumentAttribute> ComposeSendingDocumentAttributes(
 			if (document->supportsStreaming()) {
 				flags |= VideoFlag::f_supports_streaming;
 			}
+			if (document->isSilentVideo()) {
+				flags |= VideoFlag::f_nosound;
+			}
 			const auto video = document->video();
 			const auto startTs = video ? video->startTs : crl::time(0);
 			if (startTs > 0) {
@@ -109,18 +112,19 @@ MTPInputMedia PrepareUploadedDocument(
 		return MTP_inputMediaEmpty();
 	}
 	using Flag = MTPDinputMediaUploadedDocument::Flag;
+	const auto document = item->media()->document();
 	const auto spoiler = item->media() && item->media()->hasSpoiler();
 	const auto ttlSeconds = item->media()
 		? item->media()->ttlSeconds()
 		: 0;
+	const auto silent = item->groupId() || document->isSilentVideo();
 	const auto flags = (spoiler ? Flag::f_spoiler : Flag())
 		| (info.thumb ? Flag::f_thumb : Flag())
-		| (item->groupId() ? Flag::f_nosound_video : Flag())
+		| (silent ? Flag::f_nosound_video : Flag())
 		| (info.forceFile ? Flag::f_force_file : Flag())
 		| (info.attachedStickers.empty() ? Flag() : Flag::f_stickers)
 		| (ttlSeconds ? Flag::f_ttl_seconds : Flag())
 		| (info.videoCover ? Flag::f_video_cover : Flag());
-	const auto document = item->media()->document();
 	return MTP_inputMediaUploadedDocument(
 		MTP_flags(flags),
 		info.file,

Telegram/SourceFiles/boxes/edit_caption_box.cpp
@@ -1095,6 +1095,7 @@ bool EditCaptionBox::setPreparedList(Ui::PreparedList &&list) {
 		if (const auto video = std::get_if<Video>(
 				&file->information->media)) {
 			video->isGifv = false;
+			video->modifications.gif = false;
 		}
 	}
 	if (invalidForAlbum) {

Telegram/SourceFiles/editor/editor.style
@@ -228,6 +228,14 @@ videoTimelineDurationStyle: TextStyle(defaultTextStyle) {
 	font: font(12px semibold);
 }
 
+videoEditorGifButton: IconButton(photoEditorRotateButton) {
+	icon: icon {{ "photo_editor/gif", photoEditorButtonIconFg }};
+	iconOver: icon {{ "photo_editor/gif", photoEditorButtonIconFgOver }};
+}
+videoEditorGifIconActive: icon {
+	{ "photo_editor/gif", photoEditorButtonIconFgActive }
+};
+
 videoQualitySliderDotSize: 7px;
 videoQualitySliderDotActiveSize: 11px;
 videoQualitySliderLineWidth: 2px;

Telegram/SourceFiles/editor/video/video_editor.cpp
@@ -189,6 +189,7 @@ VideoEditor::VideoEditor(
 , _data(descriptor.data)
 , _initial(descriptor.initial) {
 	_geometry = _initial.geometry;
+	_gif = _initial.gif;
 	_geometry.cropType = _data.editor.cropType;
 	_geometry.cropMode = _data.editor.cropMode;
 	if (!_geometry.crop.isValid() && !_data.exactSize.isEmpty()) {
@@ -356,6 +357,12 @@ void VideoEditor::setupControls() {
 	_flip = base::make_unique_q<Ui::IconButton>(
 		bar,
 		st::photoEditorFlipButton);
+	if (!_data.removeAudio) {
+		// When the audio is dropped anyway there is nothing to choose.
+		_gifButton = base::make_unique_q<Ui::IconButton>(
+			bar,
+			st::videoEditorGifButton);
+	}
 	_confirm = base::make_unique_q<BarTextButton>(
 		bar,
 		(_data.editor.confirm.isEmpty()
@@ -486,6 +493,19 @@ void VideoEditor::setupControls() {
 		applyGeometry();
 		invalidateCoverPreview();
 	});
+	if (_gifButton) {
+		const auto refresh = [=] {
+			const auto icon = _gif
+				? &st::videoEditorGifIconActive
+				: nullptr;
+			_gifButton->setIconOverride(icon, icon);
+		};
+		refresh();
+		_gifButton->setClickedCallback([=] {
+			_gif = !_gif;
+			refresh();
+		});
+	}
 	_cancelButton->setClickedCallback([=] {
 		_cancel.fire({});
 	});
@@ -949,6 +969,7 @@ VideoModifications VideoEditor::collect() const {
 		.till = _timeline->till(),
 		.cover = _timeline->cover(),
 		.quality = _quality ? _quality->value() : 0,
+		.gif = _gif,
 	};
 }
 

Telegram/SourceFiles/editor/video/video_editor.h
@@ -93,6 +93,7 @@ private:
 	crl::time _till = 0;
 	crl::time _cover = 0;
 	crl::time _position = 0;
+	bool _gif = false;
 
 	std::unique_ptr<Media::Streaming::Instance> _instance;
 	base::unique_qptr<Crop> _crop;
@@ -102,6 +103,7 @@ private:
 	base::unique_qptr<Ui::RpWidget> _bar;
 	base::unique_qptr<Ui::IconButton> _rotate;
 	base::unique_qptr<Ui::IconButton> _flip;
+	base::unique_qptr<Ui::IconButton> _gifButton;
 	base::unique_qptr<Ui::RippleButton> _confirm;
 	base::unique_qptr<Ui::RippleButton> _cancelButton;
 	base::unique_qptr<Ui::FlatLabel> _about;

commit 6888db282ff1f4c05c2b333feffe1f541b5c1912

Fixed nosound flag dropping animated attribute from sent GIFs.
Regression was introduced in f8fe46b5a3.

Telegram/SourceFiles/api/api_media.cpp
@@ -117,7 +117,9 @@ MTPInputMedia PrepareUploadedDocument(
 	const auto ttlSeconds = item->media()
 		? item->media()->ttlSeconds()
 		: 0;
-	const auto silent = item->groupId() || document->isSilentVideo();
+	const auto animated = (document->type == AnimatedDocument);
+	const auto silent = !animated
+		&& (item->groupId() || document->isSilentVideo());
 	const auto flags = (spoiler ? Flag::f_spoiler : Flag())
 		| (info.thumb ? Flag::f_thumb : Flag())
 		| (silent ? Flag::f_nosound_video : Flag())